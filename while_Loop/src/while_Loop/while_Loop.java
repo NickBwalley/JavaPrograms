483c7ad08c761ac4a7e48fc03f92a5a59226520f
NickBwalley <nickbwalley@gmail.com>
2020-05-11 01:47:27 +0300

    java

---
+++ while_Loop/src/while_Loop/while_Loop.java
@@ -0,0 +1,13 @@
+package while_Loop;
+
+public class while_Loop {
+
+	public static void main(String[] args) {
+		int counter = 0;
+		while (counter < 10) {
+			System.out.println(counter);
+			counter ++;
+		}
+	}
+
+}
