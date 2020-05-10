9fe0c2333800e833d8981a6b68788aa99f7482f4
NickBwalley <nickbwalley@gmail.com>
2020-05-10 06:25:03 +0300

    java

---
+++ bronzeBomber/src/bronzeBomber/bronzeBomber.java
@@ -0,0 +1,10 @@
+package bronzeBomber;
+
+public class bronzeBomber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I am the bronze bomber....Deontay...Wilder!!!");
+	}
+
+}
