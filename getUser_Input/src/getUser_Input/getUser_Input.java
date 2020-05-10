7d8528e7df3954b6fb5b93886b85a0c5dc9f19b2
NickBwalley <nickbwalley@gmail.com>
2020-05-10 23:26:11 +0300

    java

---
+++ getUser_Input/src/getUser_Input/getUser_Input.java
@@ -0,0 +1,9 @@
+package getUser_Input;
+
+import java.util.Scanner;
+public class getUser_Input {
+	public static void main(String [] args) {
+		Scanner nicky = new Scanner(System.in);
+		System.out.println(nicky.nextLine());
+	}
+}
