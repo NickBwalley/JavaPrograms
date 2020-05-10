0b8aeab28e14ac39ea2ec4217e3fce698173b5c3
NickBwalley <nickbwalley@gmail.com>
2020-05-11 02:04:49 +0300

    java

---
+++ multiple_classes/src/multiple_classes/childClass.java
@@ -0,0 +1,8 @@
+package multiple_classes;
+
+public class childClass {
+	
+	public void simpleMessage() {
+		System.out.println("Hello this is another class!");
+	}
+}
---
+++ multiple_classes/src/multiple_classes/superClass.java
@@ -0,0 +1,11 @@
+package multiple_classes;
+
+public class superClass {
+
+	public static void main(String[] args) {
+		childClass newObject = new childClass();
+		newObject.simpleMessage();
+		
+	}
+
+}
