0b8b4339dc8da46238a5f3f3456766483620776c
NickBwalley <nickbwalley@gmail.com>
2020-05-11 00:26:35 +0300

    java

---
+++ DataType1/src/integers.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class integers {
+	public static void main(String[] args) {
+		int boy, girl, result;
+		boy = 49;
+		girl = 7;
+		result = boy % girl;
+		System.out.println(result);
+	}	
+
+}
