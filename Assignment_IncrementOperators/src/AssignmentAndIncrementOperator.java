19455f2abd6960da29ef1643e68b8b34b5ceebdf
NickBwalley <nickbwalley@gmail.com>
2020-05-11 00:37:43 +0300

    java

---
+++ Assignment_IncrementOperators/src/AssignmentAndIncrementOperator.java
@@ -0,0 +1,19 @@
+
+public class AssignmentAndIncrementOperator {
+
+	public static void main(String[] args) {
+		int fish = 79;
+		int lee = 99;
+		
+		lee = lee + 5;
+		System.out.println(lee);
+		lee += 9;
+		System.out.println(lee);
+		lee *= 9;
+		System.out.println(lee);
+		System.out.println(++fish);
+		System.out.println(fish++);
+		System.out.println(fish);
+	}
+
+}
