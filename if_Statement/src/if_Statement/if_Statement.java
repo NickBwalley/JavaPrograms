8677700bb93b15285bccfaee98d51fd27bf5ac9a
NickBwalley <nickbwalley@gmail.com>
2020-05-11 01:01:04 +0300

    java

---
+++ if_Statement/src/if_Statement/if_Statement.java
@@ -0,0 +1,17 @@
+package if_Statement;
+import java.util.Scanner;
+public class if_Statement {
+
+	public static void main(String[] args) {
+		Scanner rikki = new Scanner(System.in);
+		int mark;
+		System.out.println("Enter your Mark: ");
+		mark = rikki.nextInt();
+		if(mark < 50) {
+			System.out.println("Hey You have failed your Exam!.. You therefore have not qualified!");
+		}else {
+			System.out.println("Wow! Splendid! You have passed therefore you are qualified!");
+		}
+	}
+
+}
