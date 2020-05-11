aea25e18a85b023bc49d264f02578ed670b0b2c9
NickBwalley <nickbwalley@gmail.com>
2020-05-12 00:46:34 +0300

    java

---
+++ Arrays_1/src/arrays/arrays.java
@@ -0,0 +1,24 @@
+package arrays;
+
+public class arrays {
+
+	public static void main(String[] args) {
+		int nicky[] = new int[5];
+		nicky[0] = 79;
+		nicky[1] = 49;
+		nicky[2] = 59;
+		nicky[3] = 69;
+		nicky[4] = 99;
+		
+		System.out.println(nicky[0]);
+		
+		//second way of how to initialize and print arrays 
+		
+		int rikki[] = {46,30,21,41,39};
+		System.out.println(rikki[2]);
+		
+		
+		
+	}
+
+}
