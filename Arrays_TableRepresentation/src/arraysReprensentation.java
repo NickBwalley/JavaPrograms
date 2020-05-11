288e2043596d983f9991a6c6b039713b47ef8fab
NickBwalley <nickbwalley@gmail.com>
2020-05-12 00:59:09 +0300

    java

---
+++ Arrays_TableRepresentation/src/arraysReprensentation.java
@@ -0,0 +1,13 @@
+
+public class arraysReprensentation {
+
+	public static void main(String[] args) {
+		System.out.println("INDEX\tValue");
+		int nicky[] = {49,64,59,81,99,79};
+		
+		for(int i = 0; i < nicky.length; i++) {
+			System.out.println(i+"\t"+nicky[i]);
+		}
+	}
+
+}
