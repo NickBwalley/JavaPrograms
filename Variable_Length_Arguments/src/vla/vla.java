0da7aecb6f0c1eb054d0052a87c56d70e2fc1004
NickBwalley <nickbwalley@gmail.com>
2020-05-13 02:31:53 +0300

    java

--- Variable_Length_Arguments/src/vla/vla.java
+++ Variable_Length_Arguments/src/vla/vla.java
@@ -3,8 +3,16 @@ package vla;
 public class vla {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//invoking this method 
+	System.out.println("The Result is: "+ sum(12,12,12,12,12));
+	}
+	//method definitions
+	public static int sum(int...numbers) {
+		int total = 0;
+		for(int x: numbers)
+			total+=x;
+		return total;
+		
 	}
 
 }
