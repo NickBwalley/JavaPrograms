0cb3395c11413c0abc08f78bc184cb9b7584436b
NickBwalley <nickbwalley@gmail.com>
2020-05-12 23:28:58 +0300

    java

---
+++ Enhanced_For_Statement/src/EnhancedForStatement.java
@@ -0,0 +1,12 @@
+
+public class EnhancedForStatement {
+
+	public static void main(String[] args) {
+		int nicky[] = {14,16,19,224,959,779,640};
+		for(int x: nicky) {
+			System.out.println(x);
+		}
+		
+	}
+
+}
