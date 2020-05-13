4e6ef0d578dccfa0ed35c9820b8010a672dab114
NickBwalley <nickbwalley@gmail.com>
2020-05-13 04:26:44 +0300

    java

---
+++ Time_Class/src/timeClass/TimeClass.java
@@ -0,0 +1,12 @@
+package timeClass;
+
+public class TimeClass {
+
+	public static void main(String[] args) {
+		timeClass nicky = new timeClass();
+		System.out.println(nicky.toMillitary());
+		nicky.setTime(12,14,14);
+		System.out.println(nicky.toMillitary());
+	}
+
+}
