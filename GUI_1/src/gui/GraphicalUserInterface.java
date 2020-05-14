85b2cb9a8ff58d10a110fa7b9ca4535fe56027e5
NickBwalley <nickbwalley@gmail.com>
2020-05-14 06:32:56 +0300

    java

---
+++ GUI_1/src/gui/GraphicalUserInterface.java
@@ -0,0 +1,12 @@
+package gui;
+
+import java.awt.FlowLayout;//allows you to position properly what you are going to be working with 
+import javax.swing.JFrame;//imports the normal pop-up box with the minimize, maximize and close header-button
+import javax.swing.JLabel;//imports texts and images on the screen
+public class GraphicalUserInterface {
+
+	public static void main(String[] args) {
+
+	}
+
+}
