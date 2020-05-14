7da3a9d9d3214a831ae9c2aefd8c133d7cd8bed9
NickBwalley <nickbwalley@gmail.com>
2020-05-14 06:52:21 +0300

    java

--- overLoaded_Constuctors/src/overLoaded_Constuctors/OverloadedConstructors.java
+++ overLoaded_Constuctors/src/overLoaded_Constuctors/OverloadedConstructors.java
@@ -3,9 +3,16 @@ package overLoaded_Constuctors;
 public class OverloadedConstructors {
 
 	public static void main(String[] args) {
-		ConstructorOverloading nicky = new ConstructorOverloading(12,19,49);
-		//nicky.setTime(19, 49, 71);
-		System.out.println(nicky.toMillitary());
+		ConstructorOverloading nicky = new ConstructorOverloading();
+		ConstructorOverloading rikki = new ConstructorOverloading(12);
+		ConstructorOverloading cleo = new ConstructorOverloading(12,19);
+		ConstructorOverloading emma = new ConstructorOverloading(12,19,49);
+		
+		System.out.printf("%s\n", nicky.toMillitary());
+		System.out.printf("%s\n", rikki.toMillitary());
+		System.out.printf("%s\n", cleo.toMillitary());
+		System.out.printf("%s\n", emma.toMillitary());
+		
 	}
 
 }
