318d10378632ebaec159597a772c91a512c62a77
NickBwalley <nickbwalley@gmail.com>
2020-05-16 02:39:39 +0300

    java

--- GUI_1/src/gui/GraphicalUserInterface.java
+++ GUI_1/src/gui/GraphicalUserInterface.java
@@ -1,12 +1,17 @@
 package gui;
-
-import java.awt.FlowLayout;//allows you to position properly what you are going to be working with 
-import javax.swing.JFrame;//imports the normal pop-up box with the minimize, maximize and close header-button
-import javax.swing.JLabel;//imports texts and images on the screen
+import javax.swing.JOptionPane;
 public class GraphicalUserInterface {
 
 	public static void main(String[] args) {
-
+		String fNum = JOptionPane.showInputDialog("Enter First Number: ");
+		String lNum = JOptionPane.showInputDialog("Enter Second Number: ");
+		
+		int num1 = Integer.parseInt(fNum);
+		int num2 = Integer.parseInt(lNum);
+		int sum = num1 + num2;
+		
+		JOptionPane.showMessageDialog(null, "Result is: " + sum, "Addition", JOptionPane.PLAIN_MESSAGE);
+		
 	}
 
 }
