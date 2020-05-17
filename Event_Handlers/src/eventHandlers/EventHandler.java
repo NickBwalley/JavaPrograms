2675e9bf59b5c5b7a129c1e140dbbb8bb61f5e1b
NickBwalley <nickbwalley@gmail.com>
2020-05-18 01:28:50 +0300

    java

---
+++ Event_Handlers/src/eventHandlers/EventHandler.java
@@ -0,0 +1,16 @@
+package eventHandlers;
+
+public class EventHandler {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
+
+//Event handlers is the code that corresponds to your mouse movement.
+//for example Event handler will have like click a button then there will be the logic using 
+//the event handler when you click the button and what 
+//Event is what the user does 
+//Event Handler is what the program does or makes when the user does a certain operation 
+
---
+++ Event_Handlers/src/eventHandlers/NickFury.java
@@ -0,0 +1,5 @@
+package eventHandlers;
+
+public class NickFury {
+
+}
