3fa367bc8453d676083c45cbe528a8a5c1def5bd
NickBwalley <nickbwalley@gmail.com>
2020-05-19 01:16:13 +0300

    java

--- Arrays_HoldingManyObjects/src/arrays/DogList.java
+++ Arrays_HoldingManyObjects/src/arrays/DogList.java
@@ -1,5 +1,15 @@
 package arrays;
 
 public class DogList {
-
+	private Dog[] theDog = new Dog[5];
+	private int i=0;
+	
+	public void add (Dog d) {
+		if(i<theDog.length) {
+			theDog[i] = d;
+			System.out.println("Dog added at Index: " + i);
+			i++;
+		}
+	}
+	
 }
