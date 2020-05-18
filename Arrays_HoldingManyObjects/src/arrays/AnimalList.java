e603e3adfe23387c5d9e12d67d760a94fe5f48ac
NickBwalley <nickbwalley@gmail.com>
2020-05-19 01:41:11 +0300

    java

--- Arrays_HoldingManyObjects/src/arrays/AnimalList.java
+++ Arrays_HoldingManyObjects/src/arrays/AnimalList.java
@@ -4,6 +4,12 @@ public class AnimalList {
 	private Animal[] theAnimal  = new Animal[10];
 	private int x = 0;
 	
-	
+	public void add (Animal A) {
+		if(x<theAnimal.length) {
+			theAnimal[x] = A;
+			System.out.println("Animal added at Index: "+x);
+			x++;
+		}
+	}
 	
 }
