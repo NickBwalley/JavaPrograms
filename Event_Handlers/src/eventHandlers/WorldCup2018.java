66d0a8da0ae19afa25b3f61e02032eb2ed5b010a
NickBwalley <nickbwalley@gmail.com>
2020-05-18 08:19:15 +0300

    java

--- Event_Handlers/src/eventHandlers/WorldCup2018.java
+++ Event_Handlers/src/eventHandlers/WorldCup2018.java
@@ -1,5 +1,22 @@
 package eventHandlers;
 
-public class WorldCup2018 {
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.JOptionPane;
 
+public class WorldCup2018 extends JFrame {
+	private JTextField item1;
+	private JTextField item2;
+	private JTextField item3;
+	private JTextField item4;
+	
+	public WorldCup2018() {
+		super("WorldCup2018!");
+		setLayout(new FlowLayout());
+		
+	}
+	
 }
