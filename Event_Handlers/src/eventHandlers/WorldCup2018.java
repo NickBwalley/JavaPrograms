556e737bad94abec70abfe8183fbc8cb2e764225
NickBwalley <nickbwalley@gmail.com>
2020-05-18 08:27:58 +0300

    java

--- Event_Handlers/src/eventHandlers/WorldCup2018.java
+++ Event_Handlers/src/eventHandlers/WorldCup2018.java
@@ -35,6 +35,11 @@ public class WorldCup2018 extends JFrame {
 				
 	}
 	
-	
+	private class actionList implements ActionListener{
+		
+		public void actionPerformed(ActionEvent event) {
+			
+		}
+	}
 	
 }
