88c6071b8a3411eba19f534a02f6a7b8fc9bf294
NickBwalley <nickbwalley@gmail.com>
2020-05-18 07:52:08 +0300

    java

--- EventHandler/src/eventhandler/Mango.java
+++ EventHandler/src/eventhandler/Mango.java
@@ -1,6 +1,22 @@
 
 package eventhandler;
 
-public class Mango {
+import java.awt.FlowLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JOptionPane;
+
+public class Mango extends JFrame{
+    private JTextField item1;
+    private JTextField item2;
+    private JPasswordField myPassword;
     
+    public Mango(){
+        super("WrestleMania!!");
+        setLayout(new FlowLayout());
+        
+    }
 }
