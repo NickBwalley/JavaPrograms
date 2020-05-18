4d6940d5b93282a0a14dcd659e5533b939d4f1b8
NickBwalley <nickbwalley@gmail.com>
2020-05-18 07:12:30 +0300

    java

--- Polymorphism/src/polymorphism/Cat.java
+++ Polymorphism/src/polymorphism/Cat.java
@@ -1,14 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package polymorphism;
 
-/**
- *
- * @author nickbwalley
- */
-public class Cat {
-    
+public class Cat extends Animal {
+    public void eat(){
+        System.out.println("I am the Cat Class!");
+    }
 }
--- Polymorphism/src/polymorphism/Dog.java
+++ Polymorphism/src/polymorphism/Dog.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package polymorphism;
 
-/**
- *
- * @author nickbwalley
- */
-public class Dog {
-    
+
+public class Dog extends Animal {
+   public void eat(){
+        System.out.println("I am the Dog Class!");
+    }
 }
