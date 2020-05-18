e3b3a14819c4267b746fdbf073c7bdffa6130cdd
NickBwalley <nickbwalley@gmail.com>
2020-05-18 04:20:42 +0300

    java

--- Polymorphism3/src/polymorphism/Main.java
+++ Polymorphism3/src/polymorphism/Main.java
@@ -3,11 +3,11 @@ package polymorphism;
 public class Main {
 
 	public static void main(String[] args) {
-		Food fo = new Food();
+		//Food fo = new Food();
 		Rice ri = new Rice();
 		Tuna tu = new Tuna();
 		Digest dig = new Digest();
-		dig.digest(fo);
+	
 		dig.digest(ri);
 		dig.digest(tu);
 	}
@@ -16,4 +16,7 @@ public class Main {
 /*
  * Abstract Class must be overridden 
  * Abstract Class you cannot create an object out of it
+ * When you have a method inside a class the whole class needs to be labeled abstract
+ * Abstract class does not have a body
+ * Each time you have an abstract class in the super class then the subclasses need to override this abstract class
  * */
