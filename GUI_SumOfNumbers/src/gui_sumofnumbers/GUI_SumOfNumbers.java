6b5d013cf8089e4a77dc98c84e198b7c67310bc2
NickBwalley <nickbwalley@gmail.com>
2020-05-18 23:26:54 +0300

    java

--- GUI_SumOfNumbers/src/gui_sumofnumbers/GUI_SumOfNumbers.java
+++ GUI_SumOfNumbers/src/gui_sumofnumbers/GUI_SumOfNumbers.java
@@ -1,9 +1,13 @@
 
 package gui_sumofnumbers;
 
+import javax.swing.JOptionPane;
+
 public class GUI_SumOfNumbers {
 
     public static void main(String[] args) {
+        String Num1 = JOptionPane.showInputDialog("Enter First Number: ");
+        String Num2 = JOptionPane.showInputDialog("Enter Second Number: ");
         
     }
     
