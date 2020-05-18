de3c130b3d4b40e6eedc76a38218d29da530665c
NickBwalley <nickbwalley@gmail.com>
2020-05-18 03:29:44 +0300

    java

--- Polymorphism2/src/polymorphism/Polymorphism.java
+++ Polymorphism2/src/polymorphism/Polymorphism.java
@@ -13,3 +13,6 @@ public class Polymorphism {
 	}
 
 }
+/*what abstract class means is that you cannot create an object from that class but you can use 
+ *methods inside that class for inheritance and polymorphism
+ */
