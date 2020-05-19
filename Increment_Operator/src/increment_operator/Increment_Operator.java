c265b35d929158c5b675c6dcd577a3ad9be9daa0
NickBwalley <nickbwalley@gmail.com>
2020-05-19 04:43:40 +0300

    java

---
+++ Increment_Operator/src/increment_operator/Increment_Operator.java
@@ -0,0 +1,11 @@
+package increment_operator;
+
+public class Increment_Operator {
+    public static void main(String[] args) {
+    int tuna = 8;
+    int fish = 9;
+    
+        System.out.println(++tuna);//increments value before printing out the new value
+    }
+    
+}
