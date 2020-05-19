d9dc4c630a9c3c8f6a99d0e67ca48d71720bccff
NickBwalley <nickbwalley@gmail.com>
2020-05-20 01:08:42 +0300

    java

--- Math_Operations/src/math_operations/Math_Operations.java
+++ Math_Operations/src/math_operations/Math_Operations.java
@@ -6,6 +6,7 @@ public class Math_Operations {
         System.out.println(Math.abs(-23.44));//absolute value
         System.out.println(Math.ceil(14.30));//rounds off
         System.out.println(Math.floor(17.99));//rounds down
+        System.out.println(Math.pow(4,2));
     }
 
 }
