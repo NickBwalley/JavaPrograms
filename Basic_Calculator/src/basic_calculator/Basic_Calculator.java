301cb252615b87acee6af3d501c239da445339bc
NickBwalley <nickbwalley@gmail.com>
2020-05-19 04:32:02 +0300

    java

--- Basic_Calculator/src/basic_calculator/Basic_Calculator.java
+++ Basic_Calculator/src/basic_calculator/Basic_Calculator.java
@@ -1,10 +1,19 @@
 
 package basic_calculator;
 
+import java.util.Scanner;
 public class Basic_Calculator {
 
     public static void main(String[] args) {
+        Scanner nick = new Scanner(System.in);
+        double Num1, Num2, Result;
+        System.out.println("Enter First Number: ");
+        Num1 = nick.nextDouble();
+        System.out.println("Enter Second Number: ");
+        Num2 = nick.nextDouble();
         
+        Result =  Num1 + Num2;
+        System.out.println("Result = "+Result);
     }
     
 }
