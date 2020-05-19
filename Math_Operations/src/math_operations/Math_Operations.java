3145a7f4963d557d71ff46c2d607b51b6dd427fa
NickBwalley <nickbwalley@gmail.com>
2020-05-20 01:06:30 +0300

    java

--- Math_Operations/src/math_operations/Math_Operations.java
+++ Math_Operations/src/math_operations/Math_Operations.java
@@ -4,6 +4,7 @@ public class Math_Operations {
 
     public static void main(String[] args) {
         System.out.println(Math.abs(-23.44));
+        System.out.println(Math.ceil(14.30));
     }
 
 }
