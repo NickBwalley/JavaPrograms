5ac8a2d72b9b4d396e4dd66f800959bd92ad2c2f
NickBwalley <nickbwalley@gmail.com>
2020-05-20 01:01:58 +0300

    java

---
+++ DoWhile_Loop/src/dowhile_loop/DoWhile_Loop.java
@@ -0,0 +1,15 @@
+package dowhile_loop;
+
+public class DoWhile_Loop {
+//A do while loop guarantees you to do a certain operation atleast once 
+    //before you go to another operation
+    public static void main(String[] args) {
+    int counter = 11;
+    
+    do{
+        System.out.println(counter);
+        counter++;
+    }while(counter < 10);
+    }
+
+}
