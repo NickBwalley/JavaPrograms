7c1df5f793a7e2dbdb3488d1b9f58884d1b2b27c
NickBwalley <nickbwalley@gmail.com>
2020-05-19 05:08:13 +0300

    java

--- Logical_Operators/src/logical_operators/Logical_Operators.java
+++ Logical_Operators/src/logical_operators/Logical_Operators.java
@@ -1,7 +1,10 @@
 package logical_operators;
 import java.util.Scanner;
 public class Logical_Operators {
-
+    /*Logical Operators --> Logical OR: either needs to be true for the condition
+      to be true 
+      --> Logical AND: both of the operations needs to be correct for the condtion 
+        to be correct */
     public static void main(String[] args) {
         Scanner nicky = new Scanner(System.in);
         int num1, num2;
