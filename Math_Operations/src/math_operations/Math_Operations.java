1ea85120ef1f2ddea533ae209c5a49f1cb8cab5f
NickBwalley <nickbwalley@gmail.com>
2020-05-20 01:07:27 +0300

    java

--- Math_Operations/src/math_operations/Math_Operations.java
+++ Math_Operations/src/math_operations/Math_Operations.java
@@ -3,8 +3,9 @@ package math_operations;
 public class Math_Operations {
 
     public static void main(String[] args) {
-        System.out.println(Math.abs(-23.44));
-        System.out.println(Math.ceil(14.30));
+        System.out.println(Math.abs(-23.44));//absolute value
+        System.out.println(Math.ceil(14.30));//rounds off
+        System.out.println(Math.floor(17.99));//rounds down
     }
 
 }
