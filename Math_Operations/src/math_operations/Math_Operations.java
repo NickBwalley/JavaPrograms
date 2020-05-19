019ebc2924cbebd44e05decaae70522df9338ec5
NickBwalley <nickbwalley@gmail.com>
2020-05-20 01:09:59 +0300

    java

--- Math_Operations/src/math_operations/Math_Operations.java
+++ Math_Operations/src/math_operations/Math_Operations.java
@@ -6,7 +6,8 @@ public class Math_Operations {
         System.out.println(Math.abs(-23.44));//absolute value
         System.out.println(Math.ceil(14.30));//rounds off
         System.out.println(Math.floor(17.99));//rounds down
-        System.out.println(Math.pow(4,2));
+        System.out.println(Math.pow(4,2));//firstArg is the Base number secondArg is Number Raised to??
+        System.out.println(Math.sqrt(811));//gets SquareRoot of a Number
     }
 
 }
