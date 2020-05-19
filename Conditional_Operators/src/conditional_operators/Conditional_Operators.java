db585ee93f0f7d531da5dd4caf3d093dae8df15c
NickBwalley <nickbwalley@gmail.com>
2020-05-20 02:07:59 +0300

    java

--- Conditional_Operators/src/conditional_operators/Conditional_Operators.java
+++ Conditional_Operators/src/conditional_operators/Conditional_Operators.java
@@ -1,9 +1,15 @@
 package conditional_operators;
-
+import java.util.Scanner;
 public class Conditional_Operators {
 
     public static void main(String[] args) {
-    
+    Scanner nicky = new Scanner(System.in);
+    int age;
+        System.out.println("Enter Your Age: ");
+        age = nicky.nextInt();
+        //instead of adding an ifStatement you just need this conditional Operator 
+        //decision for your output or your results...
+        System.out.println(age < 18 ? "You are Still a Kid!" : "You are an Adult!");
     }
 
 }
