9de7ec4e4bd5006556ea50af50b76713de8f9782
NickBwalley <nickbwalley@gmail.com>
2020-05-20 01:24:19 +0300

    java

---
+++ ReturnMethodUsingPrintf/src/returnmethodusingprintf/ReturnMethodUsingPrintf.java
@@ -0,0 +1,9 @@
+package returnmethodusingprintf;
+
+public class ReturnMethodUsingPrintf {
+
+    public static void main(String[] args) {
+        System.out.printf("the format is %s", "NickBwalley");
+    }
+
+}
