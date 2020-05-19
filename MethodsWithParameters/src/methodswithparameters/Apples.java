a9868530a47d0ee10f16cf0f50b1c34a50cb7811
NickBwalley <nickbwalley@gmail.com>
2020-05-19 05:33:36 +0300

    java

--- MethodsWithParameters/src/methodswithparameters/Apples.java
+++ MethodsWithParameters/src/methodswithparameters/Apples.java
@@ -1,5 +1,7 @@
 package methodswithparameters;
 
 public class Apples {
-
+    public void printName(String name){
+        System.out.println("My Name is "+name);
+    }
 }
--- MethodsWithParameters/src/methodswithparameters/MethodsWithParameters.java
+++ MethodsWithParameters/src/methodswithparameters/MethodsWithParameters.java
@@ -1,9 +1,23 @@
 package methodswithparameters;
-
+import java.util.Scanner;
 public class MethodsWithParameters {
 
     public static void main(String[] args) {
-    
+        Scanner nicky = new Scanner(System.in);
+        String myName;
+        int myAge;
+        Apples apples = new Apples();
+        Lemons lemons = new Lemons();
+        
+        System.out.println("What is Your Name: ");
+        myName = nicky.nextLine();
+        System.out.println("What is Your Age: ");
+        myAge = nicky.nextInt();
+        
+        apples.printName(myName);
+        lemons.printAge(myAge);
+        
+        
     }
 
 }
