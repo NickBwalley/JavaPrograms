97418da311c2444907e7dfb35ed1c3555b5c3306
NickBwalley <nickbwalley@gmail.com>
2020-05-20 00:13:52 +0300

    java

--- whileLoopUsingAClass/src/whileloopusingaclass/WhileLoopUsingAClass.java
+++ whileLoopUsingAClass/src/whileloopusingaclass/WhileLoopUsingAClass.java
@@ -1,9 +1,13 @@
 package whileloopusingaclass;
-
+import java.util.Scanner;
 public class WhileLoopUsingAClass {
 
     public static void main(String[] args) {
-    
+        Scanner rikki = new Scanner(System.in);
+        int number;
+        Apples apples = new Apples();
+        number = rikki.nextInt();
+        apples.addNum(number);
     }
 
 }
