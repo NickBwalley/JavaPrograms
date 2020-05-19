a30b3c2e75ada93e96de31cc0faf5e2039a06f20
NickBwalley <nickbwalley@gmail.com>
2020-05-20 00:54:08 +0300

    java

--- ForLoop/src/forloop/ForLoop.java
+++ ForLoop/src/forloop/ForLoop.java
@@ -1,11 +1,21 @@
 package forloop;
-
+import java.util.Scanner;
 public class ForLoop {
 
     public static void main(String[] args) {
+    Scanner nicky = new Scanner(System.in);
+    double amount;
+    double capital;
+    double rate;
+        System.out.println("Enter Your Capital Amount: ");
+        capital = nicky.nextDouble();
+        System.out.println("Enter Your Rate of Interest: ");
+        rate = nicky.nextDouble();
         
-        for(int x=0; x<10; x++){
-            System.out.println(x);
+        for(int x=1; x<31; x++){
+            amount = capital * Math.pow(1+rate, x);
+            System.out.println("day\tAmount");
+            System.out.println(x +"\t" + amount);
         }
     }
 
