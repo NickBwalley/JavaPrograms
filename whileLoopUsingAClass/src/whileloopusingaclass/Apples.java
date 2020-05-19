8fa12bd890b944337044584adf6f9c0d0bd217be
NickBwalley <nickbwalley@gmail.com>
2020-05-20 00:06:43 +0300

    java

--- whileLoopUsingAClass/src/whileloopusingaclass/Apples.java
+++ whileLoopUsingAClass/src/whileloopusingaclass/Apples.java
@@ -2,5 +2,18 @@ package whileloopusingaclass;
 import java.util.Scanner;
 public class Apples {
 Scanner nicky = new Scanner(System.in);
+    int counter = 1;
+    int sum = 0;
     
+    
+    public int addNum(int num ){
+        while(counter < 10){
+            System.out.println("Enter Number: ");
+            num = nicky.nextInt();
+            sum+=num;
+            counter++;
+        }
+        return sum;
+    }
+
 }
