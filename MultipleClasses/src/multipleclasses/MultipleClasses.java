b7995ddf993f34d453b4dfe5e764b6a4d1ac7bda
NickBwalley <nickbwalley@gmail.com>
2020-05-19 05:22:22 +0300

    java

---
+++ MultipleClasses/src/multipleclasses/MultipleClasses.java
@@ -0,0 +1,10 @@
+package multipleclasses;
+
+public class MultipleClasses {
+
+    public static void main(String[] args) {
+        Apples iphone = new Apples();
+        iphone.printCrap();
+    }
+
+}
