b416e6db3e348306cf27b345f007a4b10efb6abc
NickBwalley <nickbwalley@gmail.com>
2020-05-20 01:11:21 +0300

    java

--- Math_Operations/src/math_operations/Math_Operations.java
+++ Math_Operations/src/math_operations/Math_Operations.java
@@ -8,6 +8,8 @@ public class Math_Operations {
         System.out.println(Math.floor(17.99));//rounds down
         System.out.println(Math.pow(4,2));//firstArg is the Base number secondArg is Number Raised to??
         System.out.println(Math.sqrt(811));//gets SquareRoot of a Number
+        System.out.println(Math.max(19.99998, 19.99999));//maximum of the TwoValues
+        
     }
 
 }
