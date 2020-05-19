5260ac25d53cc99c5137551b47e87546a9c13927
NickBwalley <nickbwalley@gmail.com>
2020-05-19 23:52:41 +0300

    java

--- computeSumOfNum_Scanner.In/src/computesumofnum_scanner/in/ComputeSumOfNum_ScannerIn.java
+++ computeSumOfNum_Scanner.In/src/computesumofnum_scanner/in/ComputeSumOfNum_ScannerIn.java
@@ -1,9 +1,21 @@
 package computesumofnum_scanner.in;
-
+import java.util.Scanner;
 public class ComputeSumOfNum_ScannerIn {
 
     public static void main(String[] args) {
+    Scanner nicky = new Scanner(System.in);
+    int counter = 1;
+    int sum = 0;
+    int num;
     
+    while(counter < 10){
+        System.out.println("Enter Number: ");
+        num = nicky.nextInt();
+        sum+=num;
+        counter++;
+    }
+    
+        System.out.println("Result is: "+ sum);
     }
 
 }
