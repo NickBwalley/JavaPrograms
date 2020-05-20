84c2f9b816a5e0525102adb9dde3aa85cdff3089
NickBwalley <nickbwalley@gmail.com>
2020-05-20 03:42:34 +0300

    java

--- Arrays/src/arrays/Arrays.java
+++ Arrays/src/arrays/Arrays.java
@@ -3,7 +3,14 @@ package arrays;
 public class Arrays {
 
     public static void main(String[] args) {
-    
+        int nick[] = new int[5];
+        nick[0] = 41;
+        nick[1] = 79;
+        nick[2] = 35;
+        nick[3] = 29;
+        nick[4] = 42;
+        
+        System.out.println(nick[1]);
     }
 
 }
