5f2cb97e2ec3ac546f3709e4d828327b1ee50ae7
NickBwalley <nickbwalley@gmail.com>
2020-05-20 05:29:34 +0300

    java

--- EnhancedForLoop/src/enhancedforloop/Main.java
+++ EnhancedForLoop/src/enhancedforloop/Main.java
@@ -3,7 +3,10 @@ package enhancedforloop;
 public class Main {
 
     public static void main(String[] args) {
-    
+    Apples apples = new Apples();
+    apples.getNumbers();
+    apples.printCrap();
+      
     }
 
 }
