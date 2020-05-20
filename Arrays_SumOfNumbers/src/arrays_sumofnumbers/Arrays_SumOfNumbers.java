1c129efdf0959c1eb7f89d05332476a4235f018d
NickBwalley <nickbwalley@gmail.com>
2020-05-20 04:24:07 +0300

    java

--- Arrays_SumOfNumbers/src/arrays_sumofnumbers/Arrays_SumOfNumbers.java
+++ Arrays_SumOfNumbers/src/arrays_sumofnumbers/Arrays_SumOfNumbers.java
@@ -3,7 +3,9 @@ package arrays_sumofnumbers;
 public class Arrays_SumOfNumbers {
 
     public static void main(String[] args) {
-    
+        NickFury rikki = new NickFury();
+        rikki.fillArray();
+        rikki.printCrap();
     }
 
 }
