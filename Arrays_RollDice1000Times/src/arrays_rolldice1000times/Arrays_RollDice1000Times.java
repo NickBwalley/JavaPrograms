417105a740e4a6c98478c5258fd51b6b6c193275
NickBwalley <nickbwalley@gmail.com>
2020-05-20 04:46:41 +0300

    java

---
+++ Arrays_RollDice1000Times/src/arrays_rolldice1000times/Arrays_RollDice1000Times.java
@@ -0,0 +1,9 @@
+package arrays_rolldice1000times;
+import java.util.Random;
+public class Arrays_RollDice1000Times {
+
+    public static void main(String[] args) {
+    Random nicky = new Random();
+    }
+
+}
