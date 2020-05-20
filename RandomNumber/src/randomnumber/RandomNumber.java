b0cdb78ad387e99e31567bbac4bac074ab454e88
NickBwalley <nickbwalley@gmail.com>
2020-05-20 03:36:22 +0300

    jaa

--- RandomNumber/src/randomnumber/RandomNumber.java
+++ RandomNumber/src/randomnumber/RandomNumber.java
@@ -1,9 +1,17 @@
 package randomnumber;
-
+import java.util.Random;
 public class RandomNumber {
 
     public static void main(String[] args) {
-    
+    Random nicky = new Random();
+        int dice;
+        
+        System.out.println("Rolls\tDiceSide");
+        
+        for(int x=1; x<=6; x++){
+            dice = 1+nicky.nextInt(6);
+            System.out.println(x + "\t" + dice);
+        }
     }
 
 }
