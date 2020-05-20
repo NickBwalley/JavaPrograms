6e4a26747ad878b3836ecac505632f0566c083fd
NickBwalley <nickbwalley@gmail.com>
2020-05-21 00:11:08 +0300

    java

--- Arrays_Multidimensional/src/arrays_multidimensional/Arrays_Multidimensional.java
+++ Arrays_Multidimensional/src/arrays_multidimensional/Arrays_Multidimensional.java
@@ -5,12 +5,17 @@ public class Arrays_Multidimensional {
     public static void main(String[] args) {
         int Apples[][] = {{11,12,13,14}, {21,22,23,24}};
         int Mango[][] = {{31,32,33,34}, {41,42,43,44}};
+        
+        printArray(Apples);
+        System.out.println("-----------------------------------");
+        printArray(Mango);
+        
     }
     //method to print out the multi-dimensional Array
     public static void printArray(int x[][]){
         for(int row=0; row<x.length; row++){
             for(int column=0; column<x[row].length; column++){
-                System.out.print(x[row][column]);
+                System.out.print(x[row][column]+ "\t");
             }
             System.out.println();
         }
