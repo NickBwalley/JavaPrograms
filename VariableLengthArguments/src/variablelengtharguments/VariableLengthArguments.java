e4d823cabe69ca9ac8255cb7d164021fc1feb4af
NickBwalley <nickbwalley@gmail.com>
2020-05-21 00:44:58 +0300

    java

--- VariableLengthArguments/src/variablelengtharguments/VariableLengthArguments.java
+++ VariableLengthArguments/src/variablelengtharguments/VariableLengthArguments.java
@@ -3,7 +3,15 @@ package variablelengtharguments;
 public class VariableLengthArguments {
 
     public static void main(String[] args) {
-    
+        System.out.println(getAverage(43,44,45));
     }
+    //Method to take Variable Length Arguments 
+   public static int getAverage(int... numbers){
+       int total = 0; 
+    //Enhanced for loop
+       for(int z: numbers)
+           total+=z;
+       return total/numbers.length;
+   }
 
 }
