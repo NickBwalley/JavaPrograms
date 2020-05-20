87a5a4a905c97ac2903f5218c96840a2710b14af
NickBwalley <nickbwalley@gmail.com>
2020-05-21 02:18:43 +0300

    java

--- TimeClass/src/timeclass/TimeClass.java
+++ TimeClass/src/timeclass/TimeClass.java
@@ -13,6 +13,10 @@ public class TimeClass {
         Time3 time3 = new Time3();
         time3.setTime(1, 12, 14);
         System.out.println(time3.toMillitary());
+        //method to cater for the use entering less than 3outputs instead of giving 
+        //3parameters as defined;
+        Time4 time4 = new Time4(5,4);
+        System.out.println(time4.getTime());
     }
 
 }
