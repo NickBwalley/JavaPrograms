50d447dcf8c44fe4e5f5c8599758f86655fd34e9
NickBwalley <nickbwalley@gmail.com>
2020-05-20 05:42:59 +0300

    java

--- Arrays_MethodInMain/src/arrays_methodinmain/Arrays_MethodInMain.java
+++ Arrays_MethodInMain/src/arrays_methodinmain/Arrays_MethodInMain.java
@@ -3,7 +3,19 @@ package arrays_methodinmain;
 public class Arrays_MethodInMain {
 
     public static void main(String[] args) {
+        int rikki[] = {29,39,49,59,79,99};
+        changeArray(rikki);
+        
+        for(int A: rikki){
+            System.out.println(A);
+        }
+    }
     
+    public static void changeArray(int x[]){
+        for(int z=0; z<x.length; z++){
+            x[z]+=5;
+            
+        }
     }
 
 }
