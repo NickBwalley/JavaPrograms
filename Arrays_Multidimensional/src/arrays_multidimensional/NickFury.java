3267c527bc2f90f88223ffd74161af460a29a5fe
NickBwalley <nickbwalley@gmail.com>
2020-05-21 00:29:00 +0300

    java

--- Arrays_Multidimensional/src/arrays_multidimensional/NickFury.java
+++ Arrays_Multidimensional/src/arrays_multidimensional/NickFury.java
@@ -1,5 +1,22 @@
 package arrays_multidimensional;
-
+import java.util.Scanner;
 public class NickFury {
-
+Scanner nicky = new Scanner(System.in);
+     
+     int rikki[][] = new int[5][5];
+     
+    //method to get user input
+    public void getInput(){
+        //creating a for-loop to allow user to enter only 10Numbers
+        for(int x=0; x<rikki.length; x++){
+            System.out.println("Enter Row-Values: ");
+            rikki[x] = nicky.nextInt();
+            for(int y=0; y<rikki[x].length; y++){
+                System.out.println("Enter Column-Values: ");
+                rikki[y] = nicky.nextInt();
+            }
+            
+        }
+        
+    }
 }
