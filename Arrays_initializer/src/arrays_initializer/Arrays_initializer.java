9e3795c02f8e3f026e54f6c2dbbbfab77f0f7945
NickBwalley <nickbwalley@gmail.com>
2020-05-20 03:53:23 +0300

    java

---
+++ Arrays_initializer/src/arrays_initializer/Arrays_initializer.java
@@ -0,0 +1,12 @@
+package arrays_initializer;
+
+public class Arrays_initializer {
+
+    public static void main(String[] args) {
+        int nick[] = {10,19,21,29,31,39};
+        //printing the elements inside your array
+        
+        
+    }
+
+}
