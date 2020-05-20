0b48e7d0c3f2f1ee64e688522318ac6c149acce4
NickBwalley <nickbwalley@gmail.com>
2020-05-21 02:43:17 +0300

    java

--- toString/src/tostring/NickFury.java
+++ toString/src/tostring/NickFury.java
@@ -5,15 +5,28 @@ public class NickFury {
     private int month;
     private int day;
     
+    public NickFury(){
+        this(1,1,1999);
+    }
+    public NickFury(int y){
+        this(y,1,1999);
+    }
+    public NickFury(int y, int m){
+        this(y,m,1999);
+    }    
     public NickFury(int y, int m, int d){
-        year = y;
-        month = m;
-        day = d;
-       //printing the values 
-        System.out.printf("The Constructor SetDate is: %s", this);
+        setYear(y);
+        setMonth(m);
+        setDay(d);
+    }
+    public void setYear(int y){
+        year = ((y>=1970 && y<2020)? y: 2020);
+    }
+    public void setMonth(int m){
+        month = ((m>=1 && m<12)? m: 1);
+    }
+    public void setDay(int d){
+        day =((d>=1 && d<32)? d: 1);
     }
     
-    public String toString(){
-        return String.format("%02d-%02d-%02d", year, month, day);
-    }
 }
