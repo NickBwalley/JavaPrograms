bc1be96845d5c968545f3613784f68b946b00277
NickBwalley <nickbwalley@gmail.com>
2020-05-20 04:20:21 +0300

    java

--- Arrays_SumOfNumbers/src/arrays_sumofnumbers/NickFury.java
+++ Arrays_SumOfNumbers/src/arrays_sumofnumbers/NickFury.java
@@ -1,5 +1,26 @@
 package arrays_sumofnumbers;
+import java.util.Scanner;
 
 public class NickFury {
-
+    Scanner nicky = new Scanner(System.in);
+    //creating our varibles
+    int total = 0;
+    //creating an array
+    int nick[] = new int[5];
+    
+    //creating a method for user input in the array
+    public void fillArray(){
+        for(int x=0; x<nick.length; x++){
+            System.out.println("Enter Number: ");
+            nick[x] = nicky.nextInt();
+        }
+    }
+    //method to sum elements inside our array
+    public void sumOfArray(){
+        for(int y=0; y<nick.length; y++){
+            total += nick[y];
+        }
+    }
+    
+    
 }
