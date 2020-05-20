299033b6389bedaae9ee6b0d2d2acc6df4f53275
NickBwalley <nickbwalley@gmail.com>
2020-05-21 01:37:37 +0300

    java

--- TimeClass/src/timeclass/TimeClass.java
+++ TimeClass/src/timeclass/TimeClass.java
@@ -3,8 +3,12 @@ package timeclass;
 public class TimeClass {
 
     public static void main(String[] args) {
-        Time1 time1 = new Time1();
-        time1.setTime();
+        //Time1 time1 = new Time1();
+        //time1.setTime();
+        Time2 time2 = new Time2();
+        time2.setTime(14,34,69);        
+        System.out.println(time2.toMillitary());
+        System.out.println(time2.toNormal());
     }
 
 }
