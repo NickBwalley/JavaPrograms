e9800226cc2c942ead8b46386a1ea8d1f8bcf129
NickBwalley <nickbwalley@gmail.com>
2020-05-20 03:45:11 +0300

    java

--- Arrays/src/arrays/Arrays.java
+++ Arrays/src/arrays/Arrays.java
@@ -9,8 +9,18 @@ public class Arrays {
         nick[2] = 35;
         nick[3] = 29;
         nick[4] = 42;
+        /*This is the first way of how to print elements of an array*/
+        //System.out.println(nick[0]);
+        //System.out.println(nick[1]);
+        //System.out.println(nick[2]);
+        //System.out.println(nick[3]);
+        //System.out.println(nick[4]);
         
-        System.out.println(nick[1]);
+        
+        /*This is the second way of printing elements of an array*/
+        for(int x=0; x<nick.length; x++){
+            System.out.println(nick[x]);
+        }
     }
 
 }
