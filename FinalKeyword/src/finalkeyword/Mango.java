bc5092923bb8579fdfd6a6d371f087e3b0cc1674
NickBwalley <nickbwalley@gmail.com>
2020-05-21 23:54:35 +0300

    java

---
+++ FinalKeyword/src/finalkeyword/Mango.java
@@ -0,0 +1,16 @@
+package finalkeyword;
+import java.util.Scanner;
+public class Mango {
+Scanner nicky = new Scanner(System.in);
+    private int sum;
+    private int userInput;
+    private final int NUMBER;
+    
+    public Mango(int number){
+        NUMBER = number;
+        System.out.println("Enter Your value: ");
+        userInput = nicky.nextInt();        
+    }
+    
+    
+}
