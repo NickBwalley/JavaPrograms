85d2ecef2572da798f49449f80e036478b71c116
NickBwalley <nickbwalley@gmail.com>
2020-05-21 04:32:08 +0300

    java

--- FinalKeyword/src/finalkeyword/FinalKeyword.java
+++ FinalKeyword/src/finalkeyword/FinalKeyword.java
@@ -3,7 +3,14 @@ package finalkeyword;
 public class FinalKeyword {
 
     public static void main(String[] args) {
-    
+        Apples apples = new Apples(10);
+        
+        for(int x=0; x<5; x++){
+            apples.add();
+            System.out.println(apples);
+            
+        }
+        
     }
 
 }
