6d8d9afc9f45aed4bb1d4ef56fb23178afbe0583
NickBwalley <nickbwalley@gmail.com>
2020-05-21 03:24:18 +0300

    java

--- toStringClass/src/tostringclass/ToStringClass.java
+++ toStringClass/src/tostringclass/ToStringClass.java
@@ -3,7 +3,9 @@ package tostringclass;
 public class ToStringClass {
 
     public static void main(String[] args) {
-        NickFury nicky = new NickFury(2,8,12);
+        NickFury nicky = new NickFury(26,8,2000);
+        Apples apples = new Apples("NickBwalley", nicky);
+        System.out.println(apples);
     }
 
 }
