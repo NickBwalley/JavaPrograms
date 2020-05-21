9d31761a907842f7c4583634ec493a342ecf115e
NickBwalley <nickbwalley@gmail.com>
2020-05-21 03:46:21 +0300

    java

--- Enumerations/src/enumerations/Apples.java
+++ Enumerations/src/enumerations/Apples.java
@@ -1,5 +1,18 @@
 package enumerations;
 
-public class Apples {
-
+public enum Apples {
+    Maimuna("Bossy Girl", "14years"),
+    Enid("Nice Girl", "16years"),
+    Ivy("Polite Girl", "17years"),
+    Chelsea("Real-Player", "19years");
+    
+    private final String desc;
+    private final String year;
+    
+    Apples(String description, String yr){
+        desc = description;
+        year = yr;
+    }
+    
+    public 
 }
