f266346cba99bdfe4a14310ad4f6ba1423811bea
NickBwalley <nickbwalley@gmail.com>
2020-05-26 14:38:43 +0300

    java

--- DSA_Practice/src/dsa_practice/DSA_Practice.java
+++ DSA_Practice/src/dsa_practice/DSA_Practice.java
@@ -3,7 +3,18 @@ package dsa_practice;
 public class DSA_Practice {
 
     public static void main(String[] args) {
+    String daysOfWeeks [] = new String[7];
+    daysOfWeeks[0] = "Monday";
+    daysOfWeeks[1] = "Tuesday";
+    daysOfWeeks[2] = "Wednesday";
+    daysOfWeeks[3] = "Thurday";
+    daysOfWeeks[4] = "Friday";
+    daysOfWeeks[5] = "Saturday";
+    daysOfWeeks[6] = "Sunday";
     
+    for(int i = 0; i < daysOfWeeks.length; i++){
+        System.out.println(daysOfWeeks[i]);
+    }
     }
 
 }
