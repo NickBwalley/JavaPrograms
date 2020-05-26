1bac52fe4fa6024cfcdf6da88d790ff360996976
NickBwalley <nickbwalley@gmail.com>
2020-05-26 23:21:25 +0300

    java

--- Recursion/src/factorial/Factorial.java
+++ Recursion/src/factorial/Factorial.java
@@ -1,9 +1,24 @@
 package factorial;
-
+import java.util.Scanner;
 public class Factorial {
 
 	public static void main(String[] args) {
-
+		Scanner nicky = new Scanner(System.in);
+		int n;
+		System.out.println("Enter Your Number: ");
+		n = nicky.nextInt();
+		findFactorial(n);
+		System.out.println("Factorial of "+n +" is "+findFactorial(n));
+	}
+	
+	//method to calculate factorial using recursion
+	public static long findFactorial(int num) {
+		//recursion must have a base case
+		if(num >= 1) {
+			return num * findFactorial(num - 1);
+		}else {
+			return 1;
+		}
 	}
 
 }
