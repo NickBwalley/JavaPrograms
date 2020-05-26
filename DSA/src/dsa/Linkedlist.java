c82c64a80d92d15d91fc3599bd79ec7722cff633
NickBwalley <nickbwalley@gmail.com>
2020-05-27 01:05:15 +0300

    java

--- DSA/src/dsa/Linkedlist.java
+++ DSA/src/dsa/Linkedlist.java
@@ -1,14 +1,93 @@
 package dsa;
 
-public class Linkedlist {
+//Java program to implement 
+//a Singly Linked List 
+public class Linkedlist { 
 
-	public static void main(String[] args) {
-		String days[] = {"Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
-		//forLoop to print the days of the Week
-		for(int x = 0; x < days.length; x++) {
-			System.out.println(days[x]);
-		}
-		System.out.println("--------------------");
-	}
+	Node head; // head of list 
 
-}
+	// Linked list Node. 
+	// This inner class is made static 
+	// so that main() can access it 
+	/*static class Node { 
+
+		int data; 
+		Node next; 
+
+		// Constructor 
+		Node(int d) 
+		{ 
+			data = d; 
+			next = null; 
+		} 
+	} */
+
+	// Method to insert a new node 
+	public static Linkedlist insert(Linkedlist list, int data) 
+	{ 
+		// Create a new node with given data 
+		Node new_node = new Node(data); 
+		new_node.next = null; 
+
+		// If the Linked List is empty, 
+		// then make the new node as head 
+		if (list.head == null) { 
+			list.head = new_node; 
+		} 
+		else { 
+			// Else traverse till the last node 
+			// and insert the new_node there 
+			Node last = list.head; 
+			while (last.next != null) { 
+				last = last.next; 
+			} 
+
+			// Insert the new_node at last node 
+			last.next = new_node; 
+		} 
+
+		// Return the list by head 
+		return list; 
+	} 
+
+	// Method to print the LinkedList. 
+	public static void printList(Linkedlist list) 
+	{ 
+		Node currNode = list.head; 
+
+		System.out.print("LinkedList: "); 
+
+		// Traverse through the LinkedList 
+		while (currNode != null) { 
+			// Print the data at current node 
+			System.out.print(currNode.data + " "); 
+
+			// Go to next node 
+			currNode = currNode.next; 
+		} 
+	} 
+
+	// Driver code 
+	public static void main(String[] args) 
+	{ 
+		/* Start with the empty list. */
+		Linkedlist list = new Linkedlist(); 
+
+		// 
+		// ******INSERTION****** 
+		// 
+
+		// Insert the values 
+		list = insert(list, 1); 
+		list = insert(list, 2); 
+		list = insert(list, 33); 
+		list = insert(list, 43); 
+		list = insert(list, 533); 
+		list = insert(list, 63); 
+		list = insert(list, 73); 
+		list = insert(list, 8); 
+
+		// Print the LinkedList 
+		printList(list); 
+	} 
+} 
