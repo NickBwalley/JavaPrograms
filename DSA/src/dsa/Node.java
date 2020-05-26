169c60f02d738c26f8f6e7ef7c43e27592b61e6a
NickBwalley <nickbwalley@gmail.com>
2020-05-26 23:13:41 +0300

    java

--- DSA/src/dsa/Node.java
+++ DSA/src/dsa/Node.java
@@ -1,11 +1,13 @@
 package dsa;
 
 public class Node {
+	Node head;//head of the list
 	int data;
-	Node address;
+	Node next;
 	
+	//construtor to create a new node and next is by default initialised as null;
 	public Node(int d) {
-		this.data = d;
-		this.address = null;
+		data = d;
 	}
+	
 }
---
+++ Recursion/src/factorial/Factorial.java
@@ -0,0 +1,9 @@
+package factorial;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+
+	}
+
+}
