ba25164213297d49fde220cc5f2d06c1eb89a728
NickBwalley <nickbwalley@gmail.com>
2020-05-27 21:14:45 +0300

    java

--- Game/src/game/Game.java
+++ Game/src/game/Game.java
@@ -1,9 +1,33 @@
 package game;
-
+import java.util.Scanner;
 public class Game {
 
     public static void main(String[] args) {
+        guessingGame();
+    }
     
+    //method guessing_Game
+    public static void guessingGame(){
+        String secret_word, guess;
+        int guess_count, guess_limit;
+   
+        secret_word = "Silver";
+        guess = "";
+        guess_count = 0;
+        guess_limit = 3;
+        
+        Scanner nicky = new Scanner(System.in);
+        System.out.println("---------------------------------------------");
+        System.out.println("Guessing-Game: Hint(Arg): You have 3Trials");
+        System.out.println("---------------------------------------------");
+        
+        while(guess!=secret_word){
+            if(guess_count < guess_limit ){
+                System.out.println("Enter Your Guess: ");
+                guess = nicky.nextLine();
+            }
+        }
+        
     }
 
 }
