70fdca25872942aa5a4cf73dd356596d2e2de9eb
NickBwalley <nickbwalley@gmail.com>
2020-05-27 21:49:26 +0300

    java

--- Game/src/game/Game.java
+++ Game/src/game/Game.java
@@ -11,7 +11,8 @@ public class Game {
         String secret_word, guess;
         int guess_count, guess_limit;
         boolean out_of_guesses;
-        secret_word = "gold";
+        secret_word = "Silver";
+        guess = "";
         guess_count = 0;
         guess_limit = 3;
         out_of_guesses = false;
@@ -19,8 +20,7 @@ public class Game {
         System.out.println("---------------------------------------------");
         System.out.println("Guessing-Game: Hint(Arg): You have 3Trials");
         System.out.println("---------------------------------------------");
-        System.out.println("Enter Your Guess: ");
-        guess = nicky.nextLine();
+        
         while(guess!=secret_word && out_of_guesses == false){
             if(guess_count < guess_limit){
                 System.out.println("Enter Your Guess: ");
