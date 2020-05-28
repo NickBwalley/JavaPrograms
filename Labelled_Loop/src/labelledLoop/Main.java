e180778e86449fa0d6f31cc1a1433a90ac2abd90
NickBwalley <nickbwalley@gmail.com>
2020-05-28 05:57:47 +0300

    java

--- Labelled_Loop/src/labelledLoop/Main.java
+++ Labelled_Loop/src/labelledLoop/Main.java
@@ -3,7 +3,20 @@ package labelledLoop;
 public class Main {
 
 	public static void main(String[] args) {
-
+		labelledLoop();
+	}
+	
+	//method for labelled loop
+	public static void labelledLoop() {
+		//outerLoop
+		for (int i = 1; i <= 3; i++) {
+			for (int j = 1; j <= 3; j++) {
+				if(i==2 && j==2) {
+					System.out.println("continue Executes for Outer Loop: ");
+				}
+				System.out.println("Value of i: "+i + " Value of j "+j);
+			}
+		}
 	}
 
 }
