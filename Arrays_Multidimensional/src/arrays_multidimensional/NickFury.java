bf83df41273d0b641ef9da2782f717dbff27fcef
NickBwalley <nickbwalley@gmail.com>
2020-06-03 22:03:11 +0300

    java

--- Arrays_Multidimensional/src/arrays_multidimensional/NickFury.java
+++ Arrays_Multidimensional/src/arrays_multidimensional/NickFury.java
@@ -1,21 +1,16 @@
 package arrays_multidimensional;
 import java.util.Scanner;
 public class NickFury {
-Scanner nicky = new Scanner(System.in);
-     
-     int rikki[][] = new int[5][5];
-     
+    
     //method to get user input
     public void getInput(){
         //creating a for-loop to allow user to enter only 10Numbers
      int twoDm[][] = new int[4][4];
     Scanner scan = new Scanner(System.in);
-    int i, j;
-
-    for (i = 0; i < 2; i++) {
+    for (int i = 0; i < 4; i++) {
         System.out.print("Enter 4 numbers seperated by comma: ");
         String[] line = scan.nextLine().split(",");
-           for (j = 0; j < 4; j++) {
+           for (int j = 0; j < 4; j++) {
             twoDm[i][j] = Integer.parseInt(line[j]);
 
             }
@@ -24,7 +19,7 @@ Scanner nicky = new Scanner(System.in);
         scan.close();
         for(int a = 0; a<twoDm.length; a++){
             for(int b = 0; b < twoDm.length; b++){
-                System.out.print(twoDm[a][b]);
+                System.out.print(twoDm[a][b]+"\t");
             }
             System.out.println("");
         }
