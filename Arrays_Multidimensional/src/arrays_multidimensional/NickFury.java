2270941a8bf134301734eecdd032c8c61b1f1ed7
NickBwalley <nickbwalley@gmail.com>
2020-06-03 05:09:09 +0300

    java

--- Arrays_Multidimensional/src/arrays_multidimensional/NickFury.java
+++ Arrays_Multidimensional/src/arrays_multidimensional/NickFury.java
@@ -8,7 +8,7 @@ Scanner nicky = new Scanner(System.in);
     //method to get user input
     public void getInput(){
         //creating a for-loop to allow user to enter only 10Numbers
-     double twoDm[][] = new double[4][4];
+     int twoDm[][] = new int[4][4];
     Scanner scan = new Scanner(System.in);
     int i, j;
 
@@ -16,17 +16,17 @@ Scanner nicky = new Scanner(System.in);
         System.out.print("Enter 4 numbers seperated by comma: ");
         String[] line = scan.nextLine().split(",");
            for (j = 0; j < 4; j++) {
-            twoDm[i][j] = Double.parseDouble(line[j]);
+            twoDm[i][j] = Integer.parseInt(line[j]);
 
             }
     }
 
         scan.close();
-        for(int a = 0; a<rikki.length; a++){
-            for(int b = 0; b < rikki.length; b++){
+        for(int a = 0; a<twoDm.length; a++){
+            for(int b = 0; b < twoDm.length; b++){
                 System.out.print(twoDm[a][b]);
             }
-            System.out.println();
+            System.out.println("");
         }
     }
 }
