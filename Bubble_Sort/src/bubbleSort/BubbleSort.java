631a0dd7bff0159a182f837a06e42a7911acfe03
NickBwalley <nickbwalley@gmail.com>
2020-06-11 02:10:44 +0300

    java

--- Bubble_Sort/src/bubbleSort/BubbleSort.java
+++ Bubble_Sort/src/bubbleSort/BubbleSort.java
@@ -7,5 +7,22 @@ public class BubbleSort {
 		int [] sampleArray = {91,79,10,61,99,48,77,90};
 		
 	}
+	
+	//method to sort an array using Bubble Sort
+	public static int [] bubbleSort(int [] arr) {
+		int n = arr.length;
+		for (int i = 1; i < n; i++) {
+			for(int j = 0; j < (n-1); j++) {
+				if(arr[j] > arr[j+1]) {
+					//swap
+					int temp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = temp;
+				}
+			}
+			
+		}
+		return arr;
+	}
 
 }
