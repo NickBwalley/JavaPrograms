a65450ef33f33963122aebaea73dcd065306e212
NickBwalley <nickbwalley@gmail.com>
2020-06-10 03:38:33 +0300

    java

--- overLoaded_Constuctors/src/overLoaded_Constuctors/TimeClass.java
+++ overLoaded_Constuctors/src/overLoaded_Constuctors/TimeClass.java
@@ -1,5 +1,45 @@
 package overLoaded_Constuctors;
 
 public class TimeClass {
-
+	private int hour;
+	private int minute;
+	private int second;
+	
+	public TimeClass() {
+		this(0,0,0);
+	}
+	public TimeClass(int h) {
+		this(h,0,0);
+	}
+	public TimeClass(int h, int m) {
+		this(h,m,0);
+	}
+	public TimeClass(int h, int m, int s) {
+		setTime(h,m,s);
+	}
+	public void setTime(int h, int m, int s) {
+		setHour(h);
+		setMinute(m);
+		setSecond(s);
+	}
+	public void setHour(int h) {
+		hour = ((h>=0 && h<24) ? h : 0);
+	}
+	public void setMinute(int m) {
+		minute = ((m>=0 && m<24) ? m : 0);
+	}
+	public void setSecond(int s) {
+		second = ((s>=0 && s<24) ? s : 0);
+	}
+	//return method to return setTime
+	public int getHour() {
+		return hour;
+	}
+	public int getMinute() {
+		return minute;
+	}
+	public int getSecond() {
+		return second;
+	}
+	
 }
