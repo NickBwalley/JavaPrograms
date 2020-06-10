d1248c3f93dfef1db3a71c146364324e1ed2a396
NickBwalley <nickbwalley@gmail.com>
2020-06-11 02:39:19 +0300

    java

--- Bubble_Sort/src/bubbleSort/BubbleSort.java
+++ Bubble_Sort/src/bubbleSort/BubbleSort.java
@@ -3,7 +3,23 @@ package bubbleSort;
 public class BubbleSort {
 
 	public static void main(String[] args) {
-
+		int [] sampleArray = {91,99,71,48,93,94,88,59,41,19,65};
+		
+	}
+	
+	//method to sort an Array using Bubble Sort
+	public static int[] bubbleSort(int [] arr) {
+		int n = arr.length;
+		for(int a = 0; a < n; a++) {
+			for(int x = 0 ; x < (n-1); x++) {
+				if(arr[x] > arr[x+1]) {
+					int temp = arr[x];
+					arr[x] = arr[x+1];
+					arr[x+1] = temp;
+				}
+			}
+		}
+		return arr;
 	}
 
 }
