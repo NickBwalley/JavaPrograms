278c3fcdcb8d6b59f44e3dec88c2d4ff487d721e
NickBwalley <nickbwalley@gmail.com>
2020-06-11 02:15:59 +0300

    java

--- Bubble_Sort/src/bubbleSort/BubbleSort.java
+++ Bubble_Sort/src/bubbleSort/BubbleSort.java
@@ -4,14 +4,19 @@ public class BubbleSort {
 
 	public static void main(String[] args) {
 		
-		int [] sampleArray = {91,79,10,61,99,48,77,90};
+		int [] sampleArray = {91,222,79,10,61,99,48,77,90};
+		int [] sortedArray = bubbleSort(sampleArray);
+		//creating a for loop to print the elements sorted in the bubble sort 
+		for(int z = 0; z < sortedArray.length; z++) {
+			System.out.printf("%s ", sortedArray[z]);
+		}
 		
 	}
 	
 	//method to sort an array using Bubble Sort
 	public static int [] bubbleSort(int [] arr) {
 		int n = arr.length;
-		for (int i = 1; i < n; i++) {
+		for (int i = 0; i < n; i++) {
 			for(int j = 0; j < (n-1); j++) {
 				if(arr[j] > arr[j+1]) {
 					//swap
