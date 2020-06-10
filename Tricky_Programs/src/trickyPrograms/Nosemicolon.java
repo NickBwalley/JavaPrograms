a8e800ba20c69e3533ca51be87980aa90cfea0ca
NickBwalley <nickbwalley@gmail.com>
2020-06-10 03:03:43 +0300

    java

--- Tricky_Programs/src/trickyPrograms/Nosemicolon.java
+++ Tricky_Programs/src/trickyPrograms/Nosemicolon.java
@@ -3,7 +3,7 @@ package trickyPrograms;
 public class Nosemicolon {
 	
 	public static void noSemicolon() {
-		if(System.out.printf("There is No Place Like: 127.0.0.1")==null) {
+		if(System.out.printf("Hello World!")==null) {
 			
 		}
 	}
--- Variable_Length_Arguments/src/vla/vla.java
+++ Variable_Length_Arguments/src/vla/vla.java
@@ -5,6 +5,7 @@ public class vla {
 	public static void main(String[] args) {
 		//invoking this method 
 	System.out.println("The Result is: "+ sum(12,12,12,12,12));
+	System.out.println("VLA Result: " + variableLengthArgs(22,33,34,34,34,34,5));
 	}
 	//method definitions
 	public static int sum(int...numbers) {
@@ -13,6 +14,15 @@ public class vla {
 			total+=x;
 		return total;
 		
+	}
+	
+	//variable length Arguments
+	public static int variableLengthArgs(int...numbers) {
+		int total = 0;
+		for(int z: numbers) {
+			total+=z;
+		}
+		return total;
 	}
 
 }
