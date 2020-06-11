cb0731d38f12b752307bffec5a5b991624d2eb91
NickBwalley <nickbwalley@gmail.com>
2020-06-11 23:57:51 +0300

    java

--- BubbleSort/src/bubblesort/BubbleSort.java
+++ BubbleSort/src/bubblesort/BubbleSort.java
@@ -3,7 +3,7 @@ package bubblesort;
 public class BubbleSort {
 
     public static void main(String[] args) {
-    
+        int[] nick = {999,491,635,797,644,323,293,966,1494,1233,1999,1697};
     }
 
 }
