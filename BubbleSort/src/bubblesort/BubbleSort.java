cc4b801b78baa45267197f2f4660f8d222ded380
NickBwalley <nickbwalley@gmail.com>
2020-06-12 00:02:19 +0300

    java

--- BubbleSort/src/bubblesort/BubbleSort.java
+++ BubbleSort/src/bubblesort/BubbleSort.java
@@ -4,6 +4,31 @@ public class BubbleSort {
 
     public static void main(String[] args) {
         int[] nick = {999,491,635,797,644,323,293,966,1494,1233,1999,1697};
+        int[] biiy = bubbleSort(nick);
+        System.out.println("The Sorted Array is: ");
+        //creating an enhanced forloop to print out the sorted array
+        for(int z: biiy){
+            System.out.printf("%s ", z);
+        }
+        System.out.println("");
+    }
+    
+    //method to sort an array using bubble sort
+    public static int[] bubbleSort(int[] arr){
+        int n;
+        n = arr.length;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < (n-1); j++){
+                if(arr[j] > arr[j+1]){
+                    int temp;
+                    temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                          
+                }
+            }
+        }
+        return arr;
     }
 
 }
