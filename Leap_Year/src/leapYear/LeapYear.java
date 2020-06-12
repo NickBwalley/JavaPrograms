5b90c57c4b976973bf2aca88b0f3461b76487e59
NickBwalley <nickbwalley@gmail.com>
2020-06-12 06:54:01 +0300

    jav

--- Leap_Year/src/leapYear/LeapYear.java
+++ Leap_Year/src/leapYear/LeapYear.java
@@ -1,31 +1,9 @@
 package leapYear;
-import java.util.Scanner;
+
 public class LeapYear {
 
 	public static void main(String[] args) {
-		Scanner nicky = new Scanner(System.in);
-		int year;
-		System.out.println("Enter A Year: ");
-		year = nicky.nextInt();
-		nicky.close();
-		
-		boolean isLeap = false;
-		//adding nested if-Statements
-		if(year % 4 == 0) {
-			if(year % 100 == 0) {
-				if(year % 400 == 0)
-					isLeap = true;
-				else
-					isLeap = false;
-			}
-			else isLeap = true;
-		}
-		else isLeap = false;
-		
-		if(isLeap == true)
-			System.out.println("Year Entered is a Leap Year!..");
-		else
-			System.out.println("Year Entered is Not a Leap Year!..");
+
 	}
 
 }
