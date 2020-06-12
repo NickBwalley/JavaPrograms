159302b70329b5adde42cf028ac4805ac0c94615
NickBwalley <nickbwalley@gmail.com>
2020-06-12 11:57:51 +0300

    java

--- ArrayLists/src/arrayList/Main.java
+++ ArrayLists/src/arrayList/Main.java
@@ -12,6 +12,10 @@ public class Main {
 		System.out.println(cars.get(1));//used to print out a value at a specific index
 		cars.set(2, "Jeep");//used to replace the value at that index to the new value defined
 		System.out.println(cars);
+		cars.remove(0);
+		System.out.println(cars);//used to remove an element from the list
+		cars.clear();//clears all the elements inside the ArrayList
+		System.out.println(cars);
 		
 	}
 
