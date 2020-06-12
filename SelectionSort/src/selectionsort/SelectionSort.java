7dcbea522461a97627e2eb547f9f777ff1f76caa
NickBwalley <nickbwalley@gmail.com>
2020-06-12 05:15:06 +0300

    java

--- SelectionSort/src/selectionsort/SelectionSort.java
+++ SelectionSort/src/selectionsort/SelectionSort.java
@@ -7,5 +7,24 @@ public class SelectionSort {
         
     }
     
-
+    //method to sort an array using SelectionSort
+    public static int[] selectionSort(int[] arr){
+        int n;
+        n = arr.length;
+        for(int i = 0; i < n; i++){
+            int minIndex = i;
+            for(int j = i; j < n; j++){
+                if(arr[j] < arr[minIndex]){
+                    minIndex = j;
+                }
+            }
+            int temp;
+            temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+        
+        return arr;
+    }
+ 
 }
