e839b6b1e7d7abe01d2b412beb837b459fe9f3b8
NickBwalley <nickbwalley@gmail.com>
2020-06-12 03:25:49 +0300

    java

--- Selection_Sort/src/selectionSort/SelectionSort.java
+++ Selection_Sort/src/selectionSort/SelectionSort.java
@@ -5,7 +5,15 @@ public class SelectionSort {
 	public static void main(String[] args) {
 		
 		int[] sampleArray = {991,466,887,523,430,221,323,640,590,499};
-		
+		System.out.println("Array before sorting is; ");
+		for(int y: sampleArray)
+			System.out.print(y + " ");
+		int[] result = selectionSort(sampleArray);
+		//creating an enhanced for loop to print the values in sorted order
+		System.out.println("\n---------------------------------------------------------");
+		System.out.println("Array after sorting is: ");
+		for(int z: result)
+			System.out.print(z + " ");
 	}
 
 	//method to sort the Array using selectionSort
@@ -15,11 +23,14 @@ public class SelectionSort {
 		for(int i = 0; i < n; i++) {
 			int minIndex = i;
 			for(int j = i; j < n; j++) {
-				if(arr[j] > arr[minIndex]) {
+				if(arr[j] < arr[minIndex]) {
 					minIndex = j;
 				}
 			}
-			
+			int temp = arr[minIndex];
+			arr[minIndex] = arr[i];
+			arr[i] = temp;
 		}
+		return arr;
 	}
 }
