b84bb07724f25c744c2b5cbd1ef2d9f14dee84c4
NickBwalley <nickbwalley@gmail.com>
2020-06-12 05:12:06 +0300

    java

---
+++ SelectionSort/src/selectionsort/SelectionSort.java
@@ -0,0 +1,11 @@
+package selectionsort;
+
+public class SelectionSort {
+
+    public static void main(String[] args) {
+        int[] sampleArray = {991,349,244,576,871,561,845};
+        
+    }
+    
+
+}
