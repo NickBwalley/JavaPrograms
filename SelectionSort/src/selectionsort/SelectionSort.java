edb0061788c4db776bc146dddf923cc4e32cb9ac
NickBwalley <nickbwalley@gmail.com>
2020-06-12 05:35:14 +0300

    java

--- SelectionSort/src/selectionsort/SelectionSort.java
+++ SelectionSort/src/selectionsort/SelectionSort.java
@@ -15,9 +15,23 @@ public class SelectionSort {
         System.out.println("Array after being sorted is: ");
         for(int z: result)
             System.out.printf("%s , ", z);
+        System.out.println("\n--------------------------------------");
+        System.out.println("The Maximum Element inside the Array is: ");
+        System.out.println(maximumElement(sampleArray));
         
     }
-    
+    //method to print out the maximum element inside our array
+    public static int maximumElement(int[] nick){
+       int n = nick.length;
+       int max = nick[0];
+       for(int i = 0; i < n; i++){
+           if(nick[i] > max){
+               max = nick[i];
+           }
+       }
+       return max;
+       
+    }
     //method to sort an array using SelectionSort
     public static int[] selectionSort(int[] arr){
         int n;
