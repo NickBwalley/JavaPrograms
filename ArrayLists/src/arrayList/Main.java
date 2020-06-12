9f0b2be7dd42a09ac5bfccc157e44576dd17cbe9
NickBwalley <nickbwalley@gmail.com>
2020-06-12 11:36:43 +0300

    java

--- ArrayLists/src/arrayList/Main.java
+++ ArrayLists/src/arrayList/Main.java
@@ -1,9 +1,14 @@
 package arrayList;
-
+import java.util.ArrayList;
 public class Main {
 
 	public static void main(String[] args) {
-
+		ArrayList<String> cars = new ArrayList<String>();
+		cars.add("Mercedes"); 
+		cars.add("BMW");
+		cars.add("Subaru");
+		cars.add("Porche Cayenne");
+		System.out.println(cars);
 	}
 
 }
