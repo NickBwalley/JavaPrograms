6386db2f2589a5ebb30cead99993b605ff5ec6b9
NickBwalley <nickbwalley@gmail.com>
2020-06-12 03:15:44 +0300

    java

--- Selection_Sort/src/selectionSort/SelectionSort.java
+++ Selection_Sort/src/selectionSort/SelectionSort.java
@@ -3,7 +3,14 @@ package selectionSort;
 public class SelectionSort {
 
 	public static void main(String[] args) {
-
+		
+		int[] sampleArray = {991,466,887,523,430,221,323,640,590,499};
+		
 	}
 
+	//method to sort the Array using selectionSort
+	public static int[] selectionSort(int[] arr) {
+		int n;
+		n = arr.length;
+	}
 }
