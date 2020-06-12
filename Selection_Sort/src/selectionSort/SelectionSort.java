e7dacd6ee98e6c3271edae9c51b66a8f7d40b749
NickBwalley <nickbwalley@gmail.com>
2020-06-12 03:19:03 +0300

    java

--- Selection_Sort/src/selectionSort/SelectionSort.java
+++ Selection_Sort/src/selectionSort/SelectionSort.java
@@ -12,5 +12,14 @@ public class SelectionSort {
 	public static int[] selectionSort(int[] arr) {
 		int n;
 		n = arr.length;
+		for(int i = 0; i < n; i++) {
+			int minIndex = i;
+			for(int j = i; j < n; j++) {
+				if(arr[j] > arr[minIndex]) {
+					minIndex = j;
+				}
+			}
+			
+		}
 	}
 }
