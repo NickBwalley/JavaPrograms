bf926835a763f61b45e5adc44d344d577870b91c
NickBwalley <nickbwalley@gmail.com>
2020-06-12 07:15:15 +0300

    java

--- Leap_Year/src/leapYear/LeapYear.java
+++ Leap_Year/src/leapYear/LeapYear.java
@@ -1,14 +1,9 @@
 package leapYear;
-import java.util.Scanner;
+
 public class LeapYear {
 
 	public static void main(String[] args) {
-		Scanner nicky = new Scanner(System.in);
-		int year;
-		System.out.println("\n---------------------\n Leap Year Checker \n----------------------");
-		System.out.println("Enter A Year: ");
-		year = nicky.nextInt();
-		
+
 	}
 
 }
