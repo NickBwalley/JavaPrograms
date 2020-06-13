e0360b9d5061769f154f5b454eb6b60180bb7cbe
NickBwalley <nickbwalley@gmail.com>
2020-06-13 10:48:08 +0300

    java

--- Concept_BreakAndContinue/src/breaksAndContinue/Main.java
+++ Concept_BreakAndContinue/src/breaksAndContinue/Main.java
@@ -14,6 +14,15 @@ public class Main {
 				break;// means that whenever the for loop reaches to the 5th element the for loop breaks 
 			System.out.printf("%d ", i);
 		}
+		System.out.println("\n------------------------");
+		int count = 0;
+		//using a while loop 
+		while(count < 10) {
+			System.out.println(count);
+			count++;
+			if(count == 4)
+				break;
+		}
 	}
 	
 	public static void keywordContinue() {
@@ -21,6 +30,20 @@ public class Main {
 			if(i == 4)
 				continue;//it means that it jumps 4 so the fourth operation is not carried out;
 			System.out.printf("%d ", i);
+		}
+		//using a while loop
+		System.out.println("\n------------------------");
+		int count = 0;
+		while(count < 10) {
+			if(count == 4) {
+				count++;
+				continue;
+				
+			}
+				
+			System.out.println(count + " ");
+			count++;
+
 		}
 	}
 
