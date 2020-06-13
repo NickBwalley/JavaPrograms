66d6ab4026a9eac72a7f02a759c745a6111497a1
NickBwalley <nickbwalley@gmail.com>
2020-06-13 10:34:41 +0300

    java

--- Concept_BreakAndContinue/src/breaksAndContinue/Main.java
+++ Concept_BreakAndContinue/src/breaksAndContinue/Main.java
@@ -3,7 +3,16 @@ package breaksAndContinue;
 public class Main {
 
 	public static void main(String[] args) {
-
+		keywordBreak();
+	}
+	
+	public static void keywordBreak() {
+		for(int i = 0; i < 10; i++) {
+			if(i==5)
+				break;
+			System.out.printf("%d ", i);
+		}
 	}
 
 }
+
