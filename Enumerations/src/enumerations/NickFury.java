43b5d5617152489e2cbda481a6905a2726cf80e9
NickBwalley <nickbwalley@gmail.com>
2020-06-13 22:48:25 +0300

    java

--- Enumerations/src/enumerations/NickFury.java
+++ Enumerations/src/enumerations/NickFury.java
@@ -1,5 +1,23 @@
 package enumerations;
 
 public enum NickFury {
-
+	DeontayWilder("The Bronze Bomber", "112kgs"),
+	TysonFury("The Gypsy King", "130kgs"),
+	MikeTyson("The Destroyer", "110kgs");
+	
+	private final String nickName;
+	private final String weights;
+	
+	NickFury(String n, String w){
+		nickName = n;
+		weights = w;
+	}
+	
+	public String getNickName() {
+		return nickName;
+	}
+	
+	public String getWeights() {
+		return weights;
+	}
 }
