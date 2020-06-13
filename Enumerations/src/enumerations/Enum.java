cc8c07d6f2ec0827267749aa960f1be0889f146c
NickBwalley <nickbwalley@gmail.com>
2020-06-13 22:57:41 +0300

    java

--- Enumerations/src/enumerations/Enum.java
+++ Enumerations/src/enumerations/Enum.java
@@ -5,6 +5,9 @@ public class Enum {
 	public static void main(String[] args) {
 		for(apples wrestlers: apples.values())
 			 System.out.printf("%s\t%s\t%s\n", wrestlers, wrestlers.getNames(), wrestlers.getWeights() );
+		System.out.println("----------------------------------");
+		for(NickFury bronzeBomber: NickFury.values())
+			System.out.printf("%s  %s  %s\n", bronzeBomber, bronzeBomber.getNickName(), bronzeBomber.getWeights());
 	}
 
 }
