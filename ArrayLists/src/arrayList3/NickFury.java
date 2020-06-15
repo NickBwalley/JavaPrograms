5611c0e83086c26feba1ba6a39b928ebf97b68bc
NickBwalley <nickbwalley@gmail.com>
2020-06-16 00:21:50 +0300

    java

--- ArrayLists/src/arrayList3/NickFury.java
+++ ArrayLists/src/arrayList3/NickFury.java
@@ -1,8 +1,26 @@
 package arrayList3;
 import java.util.Scanner;
 import java.util.ArrayList;
+
 public class NickFury {
 	public void basketBall() {
+		Scanner nicky = new Scanner(System.in);
+		ArrayList<String> players = new ArrayList<String>();
+		String name; 
+		String age;
+		String points;
+		char quit = 'Y'; //terminator character;
+		
+		while(quit == 'Y') {
+			System.out.println("Enter Players Name: ");
+			name = nicky.next();
+			System.out.println("Enter Players Age: ");
+			age = nicky.next();
+			System.out.println("Enter Players all-Time points: ");
+			points = nicky.next();
+			
+			
+		}
 		
 	}
 }
