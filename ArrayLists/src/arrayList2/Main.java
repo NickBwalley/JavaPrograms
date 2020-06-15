8a30c027b35eaa9834609e0ee4e9248aa12c5262
NickBwalley <nickbwalley@gmail.com>
2020-06-15 22:58:28 +0300

    java

--- ArrayLists/src/arrayList2/Main.java
+++ ArrayLists/src/arrayList2/Main.java
@@ -11,6 +11,27 @@ public class Main {
 	public static void classArrayList() {
 		Scanner nicky = new Scanner(System.in);
 		ArrayList<String> DBIT = new ArrayList<String>();
+		String stdNo;
+		char quit = 'Y';
+		
+		while(quit == 'Y') {
+			System.out.println("Enter Your Student-Number: ");
+			stdNo = nicky.nextLine();
+			
+			DBIT.add(stdNo);
+			
+			System.out.println("Do You Want to Enter another Record? Y/N");
+			String word;
+			word = nicky.next();
+			word = word.toUpperCase();
+			quit = word.charAt(0);
+		}
+		
+		System.out.println("The RECORDS ARE: ");
+		for(String z: DBIT)
+			System.out.println(z);
+		
+		
 		
 	}
 }
