15d2c8ad05838f68f55fba53cbca7471665f0235
NickBwalley <nickbwalley@gmail.com>
2020-06-15 23:35:20 +0300

    java

--- ArrayLists/src/arrayList2/Main.java
+++ ArrayLists/src/arrayList2/Main.java
@@ -11,23 +11,34 @@ public class Main {
 	public static void classArrayList() {
 		Scanner nicky = new Scanner(System.in);
 		ArrayList<String> DBIT = new ArrayList<String>();
+		String firstName;
+		String surName;
 		String stdNo;
+		String concat;
 		char quit = 'Y';
 		
 		while(quit == 'Y') {
-			System.out.println("Enter Your Student-Number: ");
+			System.out.println("Enter Your Sur-Name: ");
+			surName = nicky.next();
+			System.out.println("Enter Your firstName: ");
+			firstName = nicky.next();
+			System.out.println("Enter Your Students-Number: ");
 			stdNo = nicky.next();
+			concat = "---------------------------------------";
 			
+			DBIT.add(surName);
+			DBIT.add(firstName);
 			DBIT.add(stdNo);
+			DBIT.add(concat);
 			
-			System.out.println("Do You Want to Enter another Record? Y/N");
+			System.out.println("ANOTHER RECORD? Y/N");
 			String word;
 			word = nicky.next();
 			word = word.toUpperCase();
 			quit = word.charAt(0);
 		}
 		
-		System.out.println("The RECORDS ARE: ");
+		System.out.println("Records Entered are: ");
 		for(String z: DBIT)
 			System.out.println(z);
 		
