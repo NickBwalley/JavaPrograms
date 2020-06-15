b4ca2aca7c49379ca6e49fa94164b18669b60e23
NickBwalley <nickbwalley@gmail.com>
2020-06-15 22:32:35 +0300

    java

--- ArrayLists/src/arrayList2/Main.java
+++ ArrayLists/src/arrayList2/Main.java
@@ -1,9 +1,16 @@
 package arrayList2;
-
+import java.util.Scanner;
+import java.util.ArrayList;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World ");
+		
 	}
 
+	//array list 
+	public static void classArrayList() {
+		Scanner nicky = new Scanner(System.in);
+		ArrayList<String> DBIT = new ArrayList<String>();
+		
+	}
 }
