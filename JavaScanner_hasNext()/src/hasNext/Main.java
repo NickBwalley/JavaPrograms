7215e9b733dc8e8ead8bab4d86efc136760990f5
NickBwalley <nickbwalley@gmail.com>
2020-06-16 01:10:24 +0300

    java

--- JavaScanner_hasNext()/src/hasNext/Main.java
+++ JavaScanner_hasNext()/src/hasNext/Main.java
@@ -1,9 +1,22 @@
 package hasNext;
-
+import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-
+		hasNext();		
+	}
+	
+	public static void hasNext() {
+		String myName = "";
+		Scanner nicky = new Scanner(myName);
+		boolean stmt;
+		stmt = nicky.hasNext();
+		System.out.println(stmt);//returns true or false
+		nicky.close();
+	}
+	
+	public static void checkHasNext() {
+		
 	}
 	
 
