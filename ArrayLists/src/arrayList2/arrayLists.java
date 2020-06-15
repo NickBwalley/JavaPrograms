37acd875b70b555bd17453a74ec50c35253f9ce7
NickBwalley <nickbwalley@gmail.com>
2020-06-15 23:39:34 +0300

    java

---
+++ ArrayLists/src/arrayList2/arrayLists.java
@@ -0,0 +1,9 @@
+package arrayList2;
+import java.util.Scanner;
+import java.util.ArrayList;
+public class arrayLists {
+	//array list to hold names of artists and their songs 
+	public arrayLists() {
+		
+	}
+}
