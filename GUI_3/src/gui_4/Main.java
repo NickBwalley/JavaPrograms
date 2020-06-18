49397eecc0aa8d4650e9b4100973a79be358fdc1
NickBwalley <nickbwalley@gmail.com>
2020-06-19 01:13:45 +0300

    java

---
+++ GUI_3/src/gui_4/Main.java
@@ -0,0 +1,9 @@
+package gui_4;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ GUI_3/src/gui_4/Window.java
@@ -0,0 +1,17 @@
+package gui_4;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.FlowLayout;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane; 
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+
+public class Window extends JFrame {
+	private JTextField item1;
+	private JTextField item2;
+	private JTextField item3;
+	private JPasswordField password;
+	
+
+}
