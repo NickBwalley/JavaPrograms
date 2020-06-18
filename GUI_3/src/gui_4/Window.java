005bb8726b7628ee192ee6c3e5d8d59cbfbbf781
NickBwalley <nickbwalley@gmail.com>
2020-06-19 01:23:52 +0300

    java

--- GUI_3/src/gui_4/Window.java
+++ GUI_3/src/gui_4/Window.java
@@ -13,5 +13,25 @@ public class Window extends JFrame {
 	private JTextField item3;
 	private JPasswordField password;
 	
-
+	public Window() {
+		super("This is Nick");
+		setLayout(new FlowLayout());
+		
+		item1 = new JTextField("Shayna Bazsler - The kirifuda Clutch");
+		add(item1);
+		
+		item2 = new JTextField("Samoa Joe - The kokina Clutch");
+		add(item2);
+		
+		item3 = new JTextField("Drew Mcintyre - Claymore", 30);
+		item3.setEditable(false);
+		add(item3);
+		
+		password = new JPasswordField("", 10);
+		add(password);
+		
+		myHandler handler = new myHandler();
+		
+		
+	}
 }
