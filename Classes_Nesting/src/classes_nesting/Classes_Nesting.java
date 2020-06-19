c6e027d1f841eacd8eaf0b389bdb20a9d9f4d2a8
NickBwalley <nickbwalley@gmail.com>
2020-06-19 11:21:05 +0300

    java

--- Classes_Nesting/src/classes_nesting/Classes_Nesting.java
+++ Classes_Nesting/src/classes_nesting/Classes_Nesting.java
@@ -3,7 +3,19 @@ package classes_nesting;
 public class Classes_Nesting {
 
     public static void main(String[] args) {
+        getNested1();        
+    }
     
+    public static void getNested1(){
+        //first way to access nestedClasses 
+        //1. Creating an object inside the class then accessing it 
+        StoneCold nicky = new StoneCold();
+        nicky.stunner();
+        nicky.nestedClass();
+    }
+    
+    public static void getNested2(){
+        
     }
 
 }
