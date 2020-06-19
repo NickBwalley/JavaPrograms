0fa4df5ae219e6139696065ff62357560ebbef14
NickBwalley <nickbwalley@gmail.com>
2020-06-19 12:16:54 +0300

    java

--- GUI_3/src/gui_4/Main.java
+++ GUI_3/src/gui_4/Main.java
@@ -3,10 +3,10 @@ import javax.swing.JFrame;
 public class Main {
 
 	public static void main(String[] args) {
-		Window nicky = new Window();
+		NickBiiy nicky = new NickBiiy();
 		nicky.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		nicky.setSize(400,300);
-		nicky.setLocation(400, 300);
+		nicky.setLocation(400, 100);
 		nicky.setVisible(true);
 	}
 
--- GUI_3/src/gui_4/NickBiiy.java
+++ GUI_3/src/gui_4/NickBiiy.java
@@ -7,6 +7,26 @@ import java.awt.FlowLayout;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
-public class NickBiiy {
+public class NickBiiy extends JFrame {
+	private JTextField item1;
+	private JTextField entry1;
+	private JTextField item2;
+	private JPasswordField password;
+	//constructor
+	public NickBiiy() {
+		super("Students' Self-Enrollment System");
+		setLayout(new FlowLayout());
+		item1 = new JTextField("Enter Your Name: ");
+		item1.setEditable(false);
+		entry1 = new JTextField("",20);
+		item2 = new JTextField("Set Your Password");;
+		item2.setEditable(false);
+		password = new JPasswordField("",20);
+		//implementing the the ActionListener and ActionEvent
+		add(item1);
+		add(entry1);
+		add(item2);
+		add(password);
+	}
 
 }
