89295eafbbc96c6b4e7d368c60df217e4d4dc668
NickBwalley <nickbwalley@gmail.com>
2020-06-20 02:10:53 +0300

    java

--- GUI_2/src/gui_2/GUI_2.java
+++ GUI_2/src/gui_2/GUI_2.java
@@ -1,9 +1,13 @@
 package gui_2;
-
+import javax.swing.JFrame;
 public class GUI_2 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Window nicky = new Window();
+        nicky.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        nicky.setSize(400,300);
+        nicky.setLocation(400,100);
+        nicky.setVisible(true);
     }
 
 }
--- GUI_2/src/gui_2/Window.java
+++ GUI_2/src/gui_2/Window.java
@@ -2,6 +2,16 @@ package gui_2;
 import javax.swing.JFrame;
 import java.awt.FlowLayout;
 import javax.swing.JLabel;
-public class Window {
+
+public class Window extends JFrame {
+    private JLabel label1;
+    //constructor
+    public Window(){
+        super("Registration-Form");
+        setLayout(new FlowLayout());
+        label1 = new JLabel("Enter Your Name: ");
+        label1.setToolTipText("Please Enter Your Official Name: ");
+        add(label1);
+    }
     
 }
