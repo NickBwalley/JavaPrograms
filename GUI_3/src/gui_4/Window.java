34dd4c6c3cba7fda7f2b9e2f3cadab4e48234ae0
NickBwalley <nickbwalley@gmail.com>
2020-06-19 10:45:45 +0300

    java

--- GUI_3/src/gui_4/Window.java
+++ GUI_3/src/gui_4/Window.java
@@ -8,5 +8,20 @@ import javax.swing.JTextField;
 import javax.swing.JPasswordField;
 
 public class Window extends JFrame{
+	private JTextField item1;
+	private JTextField item2;
+	private JTextField item3;
+	private JPasswordField password;
 	
+	public Window() {
+		item1 = new JTextField("What is Your Name", 20);
+		item1.setEditable(false);
+		item2 = new JTextField("",10);
+		item3 = new JTextField("Set Your Password", 20);
+		item3.setEditable(false);
+		password = new JPasswordField("",10);
+		add(item1);
+		add(item2);
+		add(password);
+	}
 }
