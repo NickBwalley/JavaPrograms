6def3e6afac8140b903273f72a87727bfc738977
NickBwalley <nickbwalley@gmail.com>
2020-06-21 08:41:10 +0300

    jvaa

---
+++ NickProject1/src/nickproject1/NickProject1.java
@@ -0,0 +1,9 @@
+package nickproject1;
+
+public class NickProject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello This is the First Java Project");
+    }
+
+}
---
+++ NickProject2/src/nickproject2/NickProject2.java
@@ -0,0 +1,9 @@
+package nickproject2;
+
+public class NickProject2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World This is the Second Java Project");
+    }
+
+}
