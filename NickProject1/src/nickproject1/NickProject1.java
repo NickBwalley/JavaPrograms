209cb921914148499b5cc9bfdc13fb856002cf46
NickBwalley <nickbwalley@gmail.com>
2020-06-21 09:04:13 +0300

    java

--- NickProject1/src/nickproject1/NickProject1.java
+++ NickProject1/src/nickproject1/NickProject1.java
@@ -1,9 +1,13 @@
 package nickproject1;
-
+import DeontayWilder.BronzeBomber;
 public class NickProject1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello This is the First Java Project");
+        helloWorld();
+        BronzeBomber.iamUnbeaten();
     }
-
+    
+    public static void helloWorld(){
+        System.out.println("This is an external method and we want to call it!..");
+    }
 }
--- NickProject2/src/nickproject2/NickProject2.java
+++ NickProject2/src/nickproject2/NickProject2.java
@@ -1,9 +1,11 @@
 package nickproject2;
-
+import nickproject1.NickProject1;
 public class NickProject2 {
 
     public static void main(String[] args) {
         System.out.println("Hello World This is the Second Java Project");
+        NickProject1.helloWorld();
+        
     }
 
 }
