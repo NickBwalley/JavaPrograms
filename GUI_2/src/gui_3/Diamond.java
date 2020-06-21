99ea782b1458a0da04dfca78be4be0f7985608e3
NickBwalley <nickbwalley@gmail.com>
2020-06-21 05:03:56 +0300

    java

--- GUI_2/src/gui_3/Diamond.java
+++ GUI_2/src/gui_3/Diamond.java
@@ -20,7 +20,7 @@ public class Diamond {
         JFrame nicky = new JFrame();
         nicky.setSize(400,200);
         nicky.setLocation(400,100);
-
+        nicky.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         nicky.setTitle("Nicky's Restaurant");//super is used in first stmt of a constructor
         nicky.setLayout(new FlowLayout());
         label1 = new JLabel("Enter Your UserName: ");
@@ -38,7 +38,31 @@ public class Diamond {
         nicky.add(label3);
         nicky.add(password);
         nicky.setVisible(true);//needs to be set at the end of the last statement
+        //implementing the action listener
+        actionListener events = new actionListener();
+        username.addActionListener(events);
+        email.addActionListener(events);
+        password.addActionListener(events);
         
+    }
+    
+    //class to handle the ActionListener
+    public class actionListener implements ActionListener{
+        String biiy = "";
+        public void actionPerformed(ActionEvent event){            
+            if(event.getSource()==username)
+                biiy = String.format("Username set: %s", event.getActionCommand());
+            else if(event.getSource()==email)
+                biiy = String.format("Email set: %s", event.getActionCommand());
+            else if(event.getSource()==password)
+                biiy = String.format("Password set: %s", event.getActionCommand());
+            
+            JOptionPane.showMessageDialog(null, this);
+        }
+        
+        public String toString(){
+            return String.format(biiy, JOptionPane.PLAIN_MESSAGE);
+        }
     }
     
 }
