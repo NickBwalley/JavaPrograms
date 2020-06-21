0a84a5f407b37a58623606cf963c2ec223968303
NickBwalley <nickbwalley@gmail.com>
2020-06-21 11:28:37 +0300

    java

--- Polymorphism/src/polymorphism1/Main.java
+++ Polymorphism/src/polymorphism1/Main.java
@@ -5,8 +5,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("---------------------------");
        //Food nicky = new Food();
-        Rice rice = new Rice();
-        Beans beans = new Beans();
+        Food rice = new Rice();
+        Food beans = new Beans();
         //nicky.eat();
         rice.eat();
         beans.eat();
@@ -23,10 +23,4 @@ public class Main {
     }
 
 }
-/*	QUALITIES OF AN ABSTRACT CLASS AND ABSTRACT METHODS 
- * Abstract Class must be overridden 
- * 1. Abstract Class you cannot create an object out of it
- * 2. When you have a method inside a class the whole class needs to be labeled abstract
- * 3. Abstract class does not have a body
- * 4. Each time you have an abstract class in the super class then the subclasses need to override this abstract class
- * */
+
