78b3f005c137647ddf599d89cc10f962c1049996
NickBwalley <nickbwalley@gmail.com>
2020-06-21 03:34:12 +0300

    java

--- GUI_1/src/gui2/Apples.java
+++ GUI_1/src/gui2/Apples.java
@@ -1,5 +1,12 @@
 package gui2;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JLabel;
+import java.awt.FlowLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 public class Apples {
 	
 }
