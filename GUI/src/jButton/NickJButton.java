79a6b53284d3b368ca99af7d74debab74d78763b
NickBwalley <nickbwalley@gmail.com>
2020-06-24 07:09:33 +0300

    java

--- GUI/src/jButton/NickJButton.java
+++ GUI/src/jButton/NickJButton.java
@@ -20,5 +20,8 @@ public class NickJButton extends JFrame{
 		
 		Icon nick1 = new ImageIcon(getClass().getResource("nick1.png"));
 		Icon nick2 = new ImageIcon(getClass().getResource("nick2.png"));
+		custom = new JButton("Customized", nick1);
+		custom.setRolloverIcon(nick2);
+		add(custom);
 	}
 }
