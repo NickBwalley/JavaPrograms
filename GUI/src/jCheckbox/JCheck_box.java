5987c9772351b6237cc58357a91403799a894a53
NickBwalley <nickbwalley@gmail.com>
2020-06-29 06:57:20 +0300

    java

--- GUI/src/jCheckbox/JCheck_box.java
+++ GUI/src/jCheckbox/JCheck_box.java
@@ -3,6 +3,22 @@ import javax.swing.*;
 import java.awt.event.*;
 import java.awt.*;
 
-public class JCheck_box {
-
+public class JCheck_box extends JFrame{
+	private JTextField text1;
+	private JCheckBox boldBox;
+	private JCheckBox italicBox;
+	
+	//creating a constructor 
+	public JCheck_box() {
+		super ("Strathmore University");
+		setLayout(new FlowLayout());		
+		text1 = new JTextField("Strathmore University", 20);		
+		text1.setFont(new Font("Serif", Font.PLAIN, 14));
+		add(text1);
+		
+		boldBox = new JCheckBox("Bold");
+		italicBox = new JCheckBox("italic");
+		
+		
+	}
 }
