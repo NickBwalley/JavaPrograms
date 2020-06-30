38eb29d76bd48a2955883623781fa0e30ac8f58e
NickBwalley <nickbwalley@gmail.com>
2020-06-30 09:14:15 +0300

    java

--- GUI_2/src/Concept_1/Main.java
+++ GUI_2/src/Concept_1/Main.java
@@ -1,8 +1,13 @@
 package Concept_1;
-
+import javax.swing.JFrame;
 public class Main {
 
     public static void main(String[] args) {
+        Window1 nicky = new Window1();
+        nicky.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        nicky.setSize(400,300);
+        nicky.setLocation(400,100);
+        nicky.setVisible(true);
         
     }
 
--- GUI_2/src/Concept_1/Window1.java
+++ GUI_2/src/Concept_1/Window1.java
@@ -24,19 +24,19 @@ public class Window1 extends JFrame{
         label2 = new JLabel("Enter Your E-Mail: ");
         textfield2 = new JTextField("",20);
         label3 = new JLabel("Enter Your Password: ");
-        textfield3 = new JTextField("",20);
+        password1 = new JPasswordField("",20);
         add(label1);
-        add(label2);
-        add(label3);
         add(textfield1);
+        add(label2);
         add(textfield2);
-        add(textfield3);
+        add(label3);       
+        add(password1);
         
         // creating the actionListener
         handlerClass handler = new handlerClass();
         textfield1.addActionListener(handler);
         textfield2.addActionListener(handler);
-        textfield3.addActionListener(handler);
+        password1.addActionListener(handler);
                 
     }
     //implementing the ActionListener Class
@@ -47,7 +47,7 @@ public class Window1 extends JFrame{
                 nick = String.format("Your Name is: %s", event.getActionCommand());
             else if(event.getSource() == textfield2)
                 nick = String.format("Your Email is: %s", event.getActionCommand());
-            else if(event.getSource() == textfield3)
+            else if(event.getSource() == password1)
                 nick = String.format("Password set: %s", event.getActionCommand());
             
             JOptionPane.showMessageDialog(null, this);
