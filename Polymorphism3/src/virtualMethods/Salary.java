88e641d25de11527d195a411304b43646f2b19dd
NickBwalley <nickbwalley@gmail.com>
2020-07-21 10:36:48 +0300

    Java

--- Polymorphism3/src/virtualMethods/Salary.java
+++ Polymorphism3/src/virtualMethods/Salary.java
@@ -1,5 +1,32 @@
 package virtualMethods;
 
 public class Salary extends Employee {
-
+	
+	private double salary; //annual salary
+	public Salary(String name, String address, int number, double salary) {
+		super(name, address, number);
+		setSalary(salary);
+	}
+	
+	public void mailCheck() {
+		System.out.println("Within mailCheck of Salary Class ");
+		System.out.println("Mailing check to" + getName() + " with salary " + salary);
+	}
+	
+	public double getSalary()
+	{
+		return salary;
+	}
+	
+	public void setSalary(double newSalary)
+	{
+		if(newSalary >= 0.0){
+			salary = newSalary;
+		}
+	}
+	public double computePay() {
+		System.out.println("Computing salary pay for " + getName());
+		return salary/52;
+		
+	}
 }
