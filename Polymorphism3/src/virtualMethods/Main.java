8862e3814e86ae9016c7bc4d54dc92f7655650df
NickBwalley <nickbwalley@gmail.com>
2020-07-21 10:44:50 +0300

    Java

--- Polymorphism3/src/virtualMethods/Main.java
+++ Polymorphism3/src/virtualMethods/Main.java
@@ -3,7 +3,12 @@ package virtualMethods;
 public class Main {
 
 	public static void main(String[] args) {
-
+		Salary s = new Salary("Nick", "Bwalley", 33, 4500);
+		Employee e = new Salary("Allan", "Smith", 34, 500);
+		System.out.println("Call mailCheck using Salary reference --");
+		s.mailCheck();
+		System.out.println("\n Call mailCheck using Employee reference--");
+		e.mailCheck();
 	}
 
 }
