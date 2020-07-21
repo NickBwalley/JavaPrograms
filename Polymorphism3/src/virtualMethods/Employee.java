5e8f4958345a025a5147197b049c33dc56b9798f
NickBwalley <nickbwalley@gmail.com>
2020-07-21 10:28:44 +0300

    Java

--- Polymorphism3/src/virtualMethods/Employee.java
+++ Polymorphism3/src/virtualMethods/Employee.java
@@ -1,5 +1,30 @@
 package virtualMethods;
 
 public class Employee {
+	private String name;
+	private String address;
+	private int number;
+	//constructor
+	public Employee(String name, String address, int number) {
+		System.out.println("Constructing an Employee..");
+		this.name = name;
+		this.address = address;
+		this.number = number;
+	}
+	
+	public void mailCheck()
+	{
+		System.out.println("Mailing a Check to: " + this.name + " " + this.address);
+	}
+	
+	public String toString() {
+		return name + " " + address + " " + number;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	pu
 
 }
