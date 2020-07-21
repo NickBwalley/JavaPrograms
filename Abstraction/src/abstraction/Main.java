dd349c049f2bfcb66bafc6b36162dcf006d6e3a4
NickBwalley <nickbwalley@gmail.com>
2020-07-21 11:27:57 +0300

    Java

--- Abstraction/src/abstraction/Main.java
+++ Abstraction/src/abstraction/Main.java
@@ -7,3 +7,10 @@ public class Main {
 	}
 
 }
+/*
+ * PROPERTIES OF ABSTRACT CLASSES 
+ * 1. You cannot instantiate an abstract class.(create an instance of an object of that abstract class)
+ * 2. When you declare a method inside a class abstract, then the whole class must be declared abstract
+ * 3. You Must override all abstract methods when you inherit it in a different class
+ * */
+ 
--- Abstraction/src/abstraction/NickFury.java
+++ Abstraction/src/abstraction/NickFury.java
@@ -1,5 +1,47 @@
 package abstraction;
 
-public class NickFury {
+public abstract class NickFury {
 
+	private String name;
+	private String address;
+	private int number;
+	//constructor
+	public NickFury(String name, String address, int number) {
+		System.out.println("Constructing an Employee");
+		this.name = name;
+		this.address = address;
+		this.number = number;
+	}
+	
+	public double computePay() {
+		System.out.println("Inside Employee ComputePay");
+		return 0.0;
+	}
+	
+	public void mailCheck() {
+		System.out.println("Mailing a check to " + this.name + " " + this.address);
+	}
+	
+	public String toString() {
+		return name + " " + address + " " + number;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public String getAddress() {
+		return address;
+	}
+	
+	public void setAddress(String newAddress) {
+		newAddress = address;
+	}
+	
+	public int getNumber() {
+		return number;
+	}
+	
+	
+	
 }
