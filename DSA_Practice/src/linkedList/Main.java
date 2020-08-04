f025f25be239732f47c6b4f96d8672b8b23c4eb9
NickBwalley <nickbwalley@gmail.com>
2020-08-04 13:58:56 +0300

    java

--- DSA_Practice/src/linkedList/Main.java
+++ DSA_Practice/src/linkedList/Main.java
@@ -1,8 +1,15 @@
 package linkedList;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String [] args){
-        linkedList();
+        //linkedList();
+        int [] sampleArray = {49,12,49,14,79,99,71,12,10,94,71};
+        duplicates(sampleArray);
+        
+
     }
     
     // linked list to store the temperature reading for n number of elements
@@ -15,5 +22,18 @@ public class Main {
         animals.add("goat");
         
         System.out.print(animals + " ");
+    }
+    
+    public static List<Integer> duplicates (int a []) {
+	List<Integer> list = new ArrayList<Integer>();
+	//We loop up to size <a.length-1 since if are at 
+	//i = a.length-1 and we are checking i+1, we will get an error
+	//i.e. ArrayIndexOutOfBounds exception 
+	for (int i = 0; i < a.length-1 ; i++) {
+		if (a[i] == a[i+1]) {
+			list.add(a[i]);
+		}
+	}		
+	return list;
     }
 }
