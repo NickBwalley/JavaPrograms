5219c5f135eff67b26052cb87b964fc55ddcb979
NickBwalley <nickbwalley@gmail.com>
2020-08-06 15:57:42 +0300

    java

--- Variable_Length_Arguments/src/vla/vla.java
+++ Variable_Length_Arguments/src/vla/vla.java
@@ -5,8 +5,7 @@ public class vla {
 	public static void main(String[] args) {
 		//invoking this method 
 	System.out.println("The Result is: "+ sum(12,12,12,12,12));
-	System.out.println("VLA Result: " + variableLengthArgs(22,33,34,34,34,34,5));
-	System.out.println("The Multiplication Answer: " + multiplyNumbers(33,33));
+	//System.out.println("The Multiplication Answer: " + multiplyNumbers(33,33));
 	}
 	//method definitions
 	public static int sum(int...numbers) {
@@ -17,21 +16,4 @@ public class vla {
 		
 	}
 	
-	//variable length Arguments
-	public static int variableLengthArgs(int...numbers) {
-		int total = 0;
-		for(int z: numbers) {
-			total+=z;
-		}
-		return total;
-	}
-	
-	//variable length Arguments
-	public static int multiplyNumbers(int...numbers) {
-		int multiplication = 1;
-		for(int x: numbers)
-			multiplication*=x;
-		return multiplication;
-	}
-
 }
