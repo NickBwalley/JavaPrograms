96fa537dbd95721a50e3b4baf353566225165c30
NickBwalley <nickbwalley@gmail.com>
2020-08-06 15:53:46 +0300

    java

--- toString/src/toString/myBirthday.java
+++ toString/src/toString/myBirthday.java
@@ -1,21 +1,5 @@
 package toString;
 
 public class myBirthday {
-	private int day;
-	private int month;
-	private int year;
-	
-	//constructor 
-	public myBirthday(int d, int m, int y) {
-		day = d;
-		month = m;
-		year = y;
-		
-		System.out.printf("Your Were Born on %s", this);
-	}
-	
-	//toString method
-	public String toString() {
-		return String.format("%02d - %02d - %02d", day, month, year);
-	}
+
 }
