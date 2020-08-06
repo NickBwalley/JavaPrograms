bfa6165374690e2b9b49ffa8f27c9fa4a1b22955
NickBwalley <nickbwalley@gmail.com>
2020-08-06 15:56:21 +0300

    java

--- toString/src/toString/Main.java
+++ toString/src/toString/Main.java
@@ -3,7 +3,7 @@ package toString;
 public class Main {
 	public static void main(String[] args) {
 		ToString strowman = new ToString(14, 05, 2020);
-		//myBirthday nicky = new myBirthday(2, 9, 2000);
+		myBirthday nicky = new myBirthday(2, 9, 2000);
 	}
 
 }
--- toString/src/toString/myBirthday.java
+++ toString/src/toString/myBirthday.java
@@ -1,5 +1,19 @@
 package toString;
 
 public class myBirthday {
-
+	private int day;
+	private int month;
+	private int year;
+	//constructor
+	public myBirthday(int d, int m, int y) {
+		day = d;
+		month = m;
+		year = y;
+		
+		System.out.printf("%s\n", this);
+	}
+	// Implementing the <this> keyword inside the constructor
+	public String toString() {
+		return String.format("My Birthday is\t%d-%d-%d", day, month, year);
+	}
 }
