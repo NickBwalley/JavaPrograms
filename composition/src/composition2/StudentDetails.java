dccadfd2efd937b62c6c8a73e177a7b81b825d41
NickBwalley <nickbwalley@gmail.com>
2020-08-12 19:13:39 +0300

    java

--- composition/src/composition2/StudentDetails.java
+++ composition/src/composition2/StudentDetails.java
@@ -1,5 +1,18 @@
 package composition2;
 
 public class StudentDetails {
-
+	private String name;
+	private StudentBirthday birthday;
+	//constructor
+	public StudentDetails(String stdName, StudentBirthday stdBirthday) {
+		name = stdName;
+		birthday = stdBirthday;
+		
+		System.out.print(this);
+	}
+	
+	//implements this
+	public String toString() {
+		return String.format("Name: %s\n Birthday: %s", name, birthday);
+	}
 }
