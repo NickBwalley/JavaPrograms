44231b58741f08528bdbca67dea30bf7e5ecb44a
NickBwalley <nickbwalley@gmail.com>
2020-11-26 09:29:37 +0300

    operators

--- Operators/src/operators/Main.java
+++ Operators/src/operators/Main.java
@@ -13,6 +13,8 @@ public class Main {
 		//System.out.println(x);
 		//System.out.println(--x);
 		//System.out.println(x);
+		System.out.println(10<<2); // 10*2^2=40 // Java Left shift operator
+		System.out.println(20>>2); // 20/2^2=5	// Java Right shift operator
 		 
 	}
 
