1f15999990cd83cecd1d96a8ebca0cd20dbb8cf3
NickBwalley <nickbwalley@gmail.com>
2021-02-03 13:29:56 +0300

    core_concepts

---
+++ JavaBasics/src/javaBasics/Operators.java
@@ -0,0 +1,12 @@
+package javaBasics;
+
+public class Operators {
+	public static void main (String[] args) {
+		// operators: +, -, /, *, %
+		int  x = 10;
+		x++;
+		System.out.println(x++);
+		System.out.println(x);
+	}
+
+}
