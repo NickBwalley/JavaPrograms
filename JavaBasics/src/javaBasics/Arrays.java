a09fbdab487df1b69e13a946c83ae12d7aa5f62d
NickBwalley <nickbwalley@gmail.com>
2021-02-04 07:37:15 +0300

    java_concepts

---
+++ JavaBasics/src/javaBasics/Arrays.java
@@ -0,0 +1,18 @@
+package javaBasics;
+
+public class Arrays {
+
+	public static void main(String[] args) {
+		int [] myArray = {1,2,3,4,5}; // array initializer 
+	
+		  int [] myArray2 = new int[3];
+		  int [] myArray3 = {1,2,3,4,5};
+		  int [] myArray4 = new int[] {1,2,3,4,5};
+		  
+		  for(int i = 0; i < myArray4.length; i++) {
+			  System.out.println(myArray4[i]);
+		  }
+
+	}
+
+}
--- JavaBasics/src/javaBasics/Operators.java
+++ JavaBasics/src/javaBasics/Operators.java
@@ -4,7 +4,7 @@ public class Operators {
 	public static void main (String[] args) {
 		// operators: +, -, /, *, %
 		int  x = 10;
-		//x +=5; //x = x + 5
+		//x +=5; //x = x
 		//System.out.println(x++); //prints the value before incrementing it 
 		System.out.println(++x); //increments first the value before printing it out 
 	}
