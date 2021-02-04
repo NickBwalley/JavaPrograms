6abbe109a201951d4274f1977845932db2659dec
NickBwalley <nickbwalley@gmail.com>
2021-02-04 09:05:49 +0300

    java_condepts

--- JavaBasics/src/javaBasics/MethodsAndArgs.java
+++ JavaBasics/src/javaBasics/MethodsAndArgs.java
@@ -4,10 +4,21 @@ public class MethodsAndArgs {
 
 	public static void main(String[] args) {
 		sayHello("Nick");
-
+		sayHello("Lorna");
+		addNumbers(48,38);
+		
+		int average = averageNumbers(10,10,10);
+		System.out.println(average);
+		
 	}
 	public static void sayHello(String name) {
 		System.out.println("Hello "+ name);
+	}
+	public static void addNumbers (int a, int b) {
+		System.out.println(a+b);
+	}
+	public static int averageNumbers ( int a, int b, int c) {
+		return ((a + b + c)/3);
 	}
 
 }
