cd36a0f00c0c6c24de1ed1f982e07d23f2d3849b
NickBwalley <nickbwalley@gmail.com>
2021-02-04 08:34:55 +0300

    java_concepts

---
+++ JavaBasics/src/javaBasics/MethodsAndArgs.java
@@ -0,0 +1,13 @@
+package javaBasics;
+
+public class MethodsAndArgs {
+
+	public static void main(String[] args) {
+		sayHello("Nick");
+
+	}
+	public static void sayHello(String name) {
+		System.out.println("Hello "+ name);
+	}
+
+}
