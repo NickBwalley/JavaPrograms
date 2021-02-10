2d5502ddc54cc299f1dd71b53c77b91d86d9a6f3
NickBwalley <nickbwalley@gmail.com>
2021-02-10 08:52:56 +0300

    classes

---
+++ MyClass/src/studentClass/Main.java
@@ -0,0 +1,21 @@
+package studentClass;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Student Nick = new Student();
+		Nick.id = 49;
+		Nick.age = 20;
+		Nick.name = "Nick";
+		System.out.println(Nick.id + " " + Nick.age + " " + Nick.name);
+		
+
+	}
+
+}
+/*
+ * What is encapsulation: is the mechanism that binds together code and the data it manipulates, and keeps it
+ * both safe from outside interference and misuse
+ * What is Abstraction: is the general concept formed by extracting common features from specific examples 
+ * or the act of withdrawing or removing something unnecessary*/
+ 
---
+++ MyClass/src/studentClass/Student.java
@@ -0,0 +1,9 @@
+package studentClass;
+
+public class Student {
+		
+		int id;
+		String name;
+		int age;
+
+}
