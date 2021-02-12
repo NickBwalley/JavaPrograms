82052f6a3b2dea6e66756ef0a05e6bc5e895cfaa
NickBwalley <nickbwalley@gmail.com>
2021-02-12 12:03:31 +0300

    java_concepts

---
+++ ClassesAndMethods/src/staticKeyword/HelloStatic.java
@@ -0,0 +1,11 @@
+package staticKeyword;
+
+public class HelloStatic {
+	public static String doSomething(String message) {
+		return(message);
+	}
+	public String doSomethingElse (String message) {
+		return (message);
+	}
+}
+ 
---
+++ ClassesAndMethods/src/staticKeyword/Static.java
@@ -0,0 +1,24 @@
+package staticKeyword;
+
+public class Static {
+	//what does static keyword do in a class
+	public static void main(String[] args) {
+		//static members belong to the class instead of  a specific instance.
+		HelloStatic nick = new HelloStatic();
+	
+		System.out.println(HelloStatic.doSomething("Hello Buddies!")); //accessed via the class 
+		System.out.println(nick.doSomethingElse("Hello yeah Buddy!")); // accessed via the instance of a class 
+		System.out.println();
+		//HelloStatic nick2 = new HelloStatic();
+		//HelloStatic nick3 = new HelloStatic();
+
+	}
+
+}
+/*NOTE: 
+ * doSomething() method is a static method which means it cannot be accessed by an instance of a class
+ * doSomethingElse() method is not a static method which means it can be accessed by an instace of a class
+ * so this means that when you are trying to access the static method doSomething() you have to access it using the 
+ * class name which when you are trying to access the doSomethingElse() method which is a non-static method it means
+ * that you will have to use an instance of a class to access the method otherwise it will return a warning message*/
+ 
