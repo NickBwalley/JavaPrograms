bb43c1439c50920b0810584507fecb71395eaed3
NickBwalley <nickbwalley@gmail.com>
2021-02-15 10:24:48 +0300

    java

--- ClassesAndMethods/src/staticKeyword/Static.java
+++ ClassesAndMethods/src/staticKeyword/Static.java
@@ -17,8 +17,10 @@ public class Static {
 
 }
 /*NOTE: 
- * doSomething() method is a static method which means it cannot be accessed by an instance of a class
- * doSomethingElse() method is not a static method which means it can be accessed by an instace of a class
+ * doSomething() method is a static method which means it can be accessed by an instance of a class but it will return 
+ * an error but it is good practice and recommended that when you declare a method static use the class name to 
+ * invoke the method 
+ * doSomethingElse() method is not a static method which means it can only be accessed by an instance of a class
  * so this means that when you are trying to access the static method doSomething() you have to access it using the 
  * class name which when you are trying to access the doSomethingElse() method which is a non-static method it means
  * that you will have to use an instance of a class to access the method otherwise it will return a warning message*/
