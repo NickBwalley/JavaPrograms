996a3582ff28fed716a8dbfde00a536c6db43eb4
NickBwalley <nickbwalley@gmail.com>
2021-02-16 10:32:44 +0300

    java

--- AbstractClassesAndMethods/src/abstractClass/Main.java
+++ AbstractClassesAndMethods/src/abstractClass/Main.java
@@ -3,6 +3,8 @@ package abstractClass;
 public class Main {
 
 	public static void main(String[] args) {
+		MainBank equity = new Bank_A();
+		System.out.println(equity.getInterestRate());
 		
 
 	}
@@ -16,3 +18,7 @@ public class Main {
 //PROPERTIES OF AN ABSTRACT CLASS
 // Your cannot create an instance of an object of an abstract class but you can create a reference from
 // its derived classes (Abstract classes cannot be instantiated)
+
+//PROPERTIES OF AN ABSTRACT METHOD
+// For you to declare a method abstract, the entire class needs to be an abstract class.
+// You don't need the method body or instantiation.
