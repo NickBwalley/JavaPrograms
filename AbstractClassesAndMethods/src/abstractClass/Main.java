f5877a64427ff18fc28bb0776ce7f9e404c9cb36
NickBwalley <nickbwalley@gmail.com>
2021-02-16 10:24:04 +0300

    java

---
+++ AbstractClassesAndMethods/src/abstractClass/Main.java
@@ -0,0 +1,18 @@
+package abstractClass;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
+
+//An Abstract class is a class which cannot be instantiated
+// SuperClass (Base Class, Parent Class)
+// SubClass (Derived Class, Child Class)
+
+//PROPERTIES OF AN ABSTRACT CLASS
+// Your cannot create an instance of an object of an abstract class but you can create a reference from
+// its derived classes (Abstract classes cannot be instantiated)
