d49b34dc12759a49b80c182151751381760bd7ec
NickBwalley <nickbwalley@gmail.com>
2021-02-16 12:49:23 +0300

    java

--- ArrayListsAndLinkedLists/src/linkedLists/Main.java
+++ ArrayListsAndLinkedLists/src/linkedLists/Main.java
@@ -1,9 +1,27 @@
 package linkedLists;
 
+import java.util.LinkedList;
+
 public class Main {
 
 	public static void main(String[] args) {
+		LinkedList<String> linkedList = new LinkedList<String>();
+		linkedList.add("Lorna");
+		linkedList.add("Nicky");
+		linkedList.add("Solomon");
+		linkedList.add("Brian");
+		linkedList.addFirst("KingKong"); // adds an elements top of the list
+		linkedList.addLast("TheLastRide"); // adds an element last of the list
+		linkedList.removeFirst(); // remove the first element of the list
+		linkedList.removeLast(); // remove the last element of the list 
+		linkedList.add(0, "JokerSmoker");
+		linkedList.set(1, "Nicholas");
 		
+		//enhanced for loop
+		for(String x: linkedList)
+			System.out.println(x);
+		System.out.println("size = " + linkedList.size());
+		linkedList.clear(); // clears all the element of the list 
 
 	}
 
