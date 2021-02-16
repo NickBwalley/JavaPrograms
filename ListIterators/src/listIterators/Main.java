9fe08db393dc5fe3df764b2230f76b72c0f89ec9
NickBwalley <nickbwalley@gmail.com>
2021-02-16 15:57:27 +0300

    java

--- ListIterators/src/listIterators/Main.java
+++ ListIterators/src/listIterators/Main.java
@@ -1,10 +1,29 @@
 package listIterators;
+/*
+ * public boolean hasNext()
+ * public Object next()
+ * public boolean hasPrevious()
+ * public object previous()
+ * */
+import java.util.*;
 
 public class Main {
 
 	public static void main(String[] args) {
+		ArrayList<String> names = new ArrayList<String>();
+		names.add("Nicholas");
+		names.add("Lorna");
+		names.add("Solomon");
+		names.add("Brian");
+		names.add("David");
 		
-
+		ListIterator<String> itr = names.listIterator();
+		while(itr.hasNext())
+			System.out.println(itr.next());
+		System.out.println("--------------------");
+		while(itr.hasPrevious())
+			System.out.println(itr.previous());
+		
 	}
 
 }
