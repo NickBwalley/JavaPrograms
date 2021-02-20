7662f671dd45e1f092326730d670d9855de4c8cf
NickBwalley <nickbwalley@gmail.com>
2021-02-20 11:20:08 +0300

    hashSet

--- HashSet/src/hashSet/Main.java
+++ HashSet/src/hashSet/Main.java
@@ -1,10 +1,22 @@
 package hashSet;
 
+import java.util.*;
+
 public class Main {
 
 	public static void main(String[] args) {
+		HashSet<String> name = new HashSet<String>();
+		name.add("Nicholas");
+		name.add("Lorna");
+		name.add("Solomon");
+		name.add("David");
+		name.add("Brian");
+		name.add("Smith");
 		
-
+		Iterator<String> itr = name.iterator(); // prints the list in forward direction
+		while(itr.hasNext())
+			System.out.println(itr.next()); // prints the value in forward direction
+		
 	}
 
 }
