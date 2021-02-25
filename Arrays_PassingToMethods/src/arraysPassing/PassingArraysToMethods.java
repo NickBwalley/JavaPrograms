07081f34a72d5601801fafb9cb6c2181c312d6f6
NickBwalley <nickbwalley@gmail.com>
2021-02-25 13:09:59 +0300

    passingArrayToMethods

--- Arrays_PassingToMethods/src/arraysPassing/PassingArraysToMethods.java
+++ Arrays_PassingToMethods/src/arraysPassing/PassingArraysToMethods.java
@@ -6,33 +6,33 @@ public class PassingArraysToMethods {
 
 	public static void main(String[] args) {
 		Scanner nicky = new Scanner(System.in);
-//		int rikki[] = new int[5];
-//		 
-//			System.out.println("Enter First Number: ");
-//			rikki[0] = nicky.nextInt();
-//			System.out.println("Enter Second Number: ");
-//			rikki[1] = nicky.nextInt();
-//			System.out.println("Enter Third Number: ");
-//			rikki[2] = nicky.nextInt();
-//			System.out.println("Enter Fourth Number: ");
-//			rikki[3] = nicky.nextInt();
-//			System.out.println("Enter Fifth Number: ");
-//			rikki[4] = nicky.nextInt();
-//		
-//		
-//		change(rikki);
-//		for(int n: rikki) {
-//			System.out.println(n);
-//		}
-//		
+		int rikki[] = new int[5];
+		 
+			System.out.println("Enter First Number: ");
+			rikki[0] = nicky.nextInt();
+			System.out.println("Enter Second Number: ");
+			rikki[1] = nicky.nextInt();
+			System.out.println("Enter Third Number: ");
+			rikki[2] = nicky.nextInt();
+			System.out.println("Enter Fourth Number: ");
+			rikki[3] = nicky.nextInt();
+			System.out.println("Enter Fifth Number: ");
+			rikki[4] = nicky.nextInt();
 		
-		Prickler bwalley  = new Prickler();
-		bwalley.passArray();
+		
+		change(rikki);
+		for(int n: rikki) {
+			System.out.println(n);
+		}
+		nicky.close();
+		
+//		Prickler bwalley  = new Prickler();
+//		bwalley.passArray();
 	}
 	
 		public static void change(int x[]) {//passing an array into a method
 			for(int z=0; z<x.length; z++)
-				x[z]+=1;
+				x[z]+=1; // adds one to each element inside the array
 		}
 	
 }
--- Arrays_PassingToMethods/src/arraysPassing/Prickler.java
+++ Arrays_PassingToMethods/src/arraysPassing/Prickler.java
@@ -4,13 +4,13 @@ import java.util.Scanner;
 public class Prickler {
 	
 	private static int counter = 1;
-	Scanner scurry = new Scanner(System.in);
+	Scanner input = new Scanner(System.in);
 	int nick[] = new int[5];
 	//method to populate an array
 	public void passArray() {
 		for (int i = 0; i < nick.length; i++) {
 			System.out.println("Enter Element " + counter + ": " );
-			nick[i] = scurry.nextInt();
+			nick[i] = input.nextInt();
 			counter ++;
 		}
 		System.out.println("This are the Elements inside Your array: ");
