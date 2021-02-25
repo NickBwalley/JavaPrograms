d2170344fa49fc73114b3e7d4108ca0df0dd62cf
NickBwalley <nickbwalley@gmail.com>
2021-02-25 11:58:41 +0300

    arrayTableRepresentation

--- Arrays_TableRepresentation/src/tableArray/arraysReprensentation.java
+++ Arrays_TableRepresentation/src/tableArray/arraysReprensentation.java
@@ -1,9 +1,10 @@
+package tableArray;
 
 public class arraysReprensentation {
 
 	public static void main(String[] args) {
 		System.out.println("INDEX\tValue");
-		int nicky[] = {49,64,59,81,99,79};
+		int [] nicky = {49,64,59,81,99,79}; // Array Initializer
 		
 		for(int i = 0; i < nicky.length; i++) {
 			System.out.println(i+"\t"+nicky[i]);
