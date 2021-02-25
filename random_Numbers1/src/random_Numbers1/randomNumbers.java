51e5c3adb68493e4c1a4292156555613a699b0eb
NickBwalley <nickbwalley@gmail.com>
2021-02-25 11:42:21 +0300

    randomNumberGenerator

--- random_Numbers1/src/random_Numbers1/randomNumbers.java
+++ random_Numbers1/src/random_Numbers1/randomNumbers.java
@@ -3,10 +3,14 @@ import java.util.Random;
 public class randomNumbers {
 
 	public static void main(String[] args) {
-		Random nicky = new Random();
+		generateRandNum();
+	}
+	
+	public static void generateRandNum() {
+		Random dice = new Random();
 		int number;
-		for(int i=0; i<5; i++) {
-			number = 1+nicky.nextInt(6);
+		for(int i = 0; i < 5; i++) {
+			number = 1+dice.nextInt(10);
 			System.out.println(number);
 		}
 	}
