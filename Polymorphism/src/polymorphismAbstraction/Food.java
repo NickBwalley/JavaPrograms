96ff95a45b7db451b23aabd24f448a35a471bbef
NickBwalley <nickbwalley@gmail.com>
2021-03-03 10:33:51 +0300

    polymorphism

--- Polymorphism/src/polymorphismAbstraction/Food.java
+++ Polymorphism/src/polymorphismAbstraction/Food.java
@@ -1,6 +1,6 @@
 package polymorphismAbstraction;
 
-abstract public class Food {
+public abstract class Food {
 	public abstract void eat();
 }
 //Abstract class means that you cannot create an object out of this class then use it 
