14a341c9ebfac37d578983eba688d175ef4f996b
NickBwalley <nickbwalley@gmail.com>
2021-03-03 11:46:48 +0300

    toStringMethod

---
+++ toString/src/toString2/Main.java
@@ -0,0 +1,13 @@
+package toString2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// creating an integer of value 10
+		Integer number = 10;
+		// calling the toString method as a function of the Integer
+		System.out.println(number.toString());
+
+	}
+
+}
