6f6b9e764695583147fa8da1a523d0cd545a54d0
NickBwalley <nickbwalley@gmail.com>
2021-03-03 10:30:52 +0300

    methodOverriding

--- Overriding/src/overRiding/Main.java
+++ Overriding/src/overRiding/Main.java
@@ -13,3 +13,8 @@ public class Main {
 	}
 
 }
+/*OVERRIDING RULES:
+ * When you override a method from base class it needs to have the same number of arguments and return type
+ * When you override a method from base class it must have the same access modifier as it is in the base class
+ * *
+ */
