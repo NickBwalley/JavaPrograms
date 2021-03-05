fb5360543b3eb32b62d4c56253c35cc29792c873
NickBwalley <nickbwalley@gmail.com>
2021-03-05 11:43:27 +0300

    dataTypes

---
+++ DataTypes/src/dataTypes/Main.java
@@ -0,0 +1,25 @@
+package dataTypes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int a = 49;
+		double b = 4e5;
+		System.out.println(a);
+		System.out.println(b);
+
+	}
+
+}
+//DataType	Size	Description
+//byte		1 byte	Stores whole numbers from -128 to 127
+//short		2 bytes	Stores whole numbers from -32,768 to 32,767
+//int		4 bytes	Stores whole numbers from -2,147,483,648 to 2,147,483,647
+//long		8 bytes	Stores whole numbers from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+//float		4 bytes	Stores fractional numbers. Sufficient for storing 6 to 7 decimal digits
+//double	8 bytes	Stores fractional numbers. Sufficient for storing 15 decimal digits
+//boolean	1 bit	Stores true or false values
+//char		2 bytes	Stores a single character/letter or ASCII values
+
+//Primitive data types: The primitive data types include boolean, char, byte, short, int, long, float and double.
+//Non-primitive data types: The non-primitive data types include Classes, Interfaces, and Arrays.
