0c581a5e8da3e891b4b43f938f9ec0e2475dd863
NickBwalley <nickbwalley@gmail.com>
2021-03-11 10:00:50 +0300

    typeCasting

--- TypeCasting/src/typeCasting/Main.java
+++ TypeCasting/src/typeCasting/Main.java
@@ -10,8 +10,26 @@ package typeCasting;
 public class Main {
 
 	public static void main(String[] args) {
-		
-
+		//wideningCasting();
+		new Main().narrowingCasting();
 	}
+	
+	// Widening casting is done automatically when passing a smaller size type to a larger size type:
+	public static void wideningCasting() {
+	    int myInt = 9;
+	    double myDouble = myInt; // Automatic casting: int to double
 
+	    System.out.println(myInt);      // Outputs 9
+	    System.out.println(myDouble);   // Outputs 9.0
+	}
+	
+	// Narrowing casting must be done manually by placing the type in parentheses in front of the value:
+	public void narrowingCasting() {
+	    double myDouble = 9.78;
+	    int myInt = (int) myDouble; // Manual casting: double to int
+
+	    System.out.println(myDouble);   // Outputs 9.78
+	    System.out.println(myInt);      // Outputs 9
+	}
+	
 }
