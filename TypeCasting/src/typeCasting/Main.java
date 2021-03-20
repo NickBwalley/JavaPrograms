a9a4ab9d78b00bde2fb140a2a535af1ba13edffe
NickBwalley <nickbwalley@gmail.com>
2021-03-20 15:54:37 +0300

    typecasting

--- TypeCasting/src/typeCasting/Main.java
+++ TypeCasting/src/typeCasting/Main.java
@@ -23,7 +23,7 @@ public class Main {
 	    System.out.println(myDouble);   // Outputs 9.0
 	}
 	
-	// Narrowing casting must be done manually by placing the type in parentheses in front of the value:
+	// Narrowing casting must be done manually by placing the type in parentheses before the value:
 	public void narrowingCasting() {
 	    double myDouble = 9.78;
 	    int myInt = (int) myDouble; // Manual casting: double to int
@@ -33,3 +33,16 @@ public class Main {
 	}
 	
 }
+//DataType	Size	Description
+//byte		1 byte	Stores whole numbers from -128 to 127
+//short		2 bytes	Stores whole numbers from -32,768 to 32,767
+//int		4 bytes	Stores whole numbers from -2,147,483,648 to 2,147,483,647
+//long		8 bytes	Stores whole numbers from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+//float		4 bytes	Stores fractional numbers. Sufficient for storing 6 to 7 decimal digits
+//double	8 bytes	Stores fractional numbers. Sufficient for storing 15 decimal digits
+//boolean	1 bit	Stores true or false values
+//char		2 bytes	Stores a single character/letter or ASCII values
+
+//Primitive data types: The primitive data types include boolean, char, byte, short, int, long, float and double.
+//Non-primitive data types: The non-primitive data types include Classes, Interfaces, and Arrays.
+
