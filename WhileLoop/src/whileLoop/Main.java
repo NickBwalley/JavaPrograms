372b6c612fa77c699888873063bfa0799f6b5802
NickBwalley <nickbwalley@gmail.com>
2021-03-21 12:00:31 +0300

    while_loop

---
+++ WhileLoop/src/whileLoop/Main.java
@@ -0,0 +1,14 @@
+package whileLoop;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int counter = 0;
+		while (counter < 10) {
+			System.out.println(counter);
+			counter ++; // Note: Do not forget to increase the variable used in the condition, 
+							//otherwise the loop will never end!
+		}
+	}
+
+}
