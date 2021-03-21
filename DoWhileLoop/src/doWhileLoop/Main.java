f48b4e966077afca63e7b818ed4fec2b096d977e
NickBwalley <nickbwalley@gmail.com>
2021-03-21 12:03:12 +0300

    do_while_loop

--- DoWhileLoop/src/doWhileLoop/Main.java
+++ DoWhileLoop/src/doWhileLoop/Main.java
@@ -1,8 +1,8 @@
-package doWhile_Loop;
+package doWhileLoop;
 //concept to understand is that a doWhile loop guarantees you to do a certain operation at least once
 //unlike the while loop which checks the condition before executing a certain operation, a do while loop
 //first loops at least once before checking the operation if it is true or false
-public class doWhile {
+public class Main {
 	public static void main(String[] args) {
 		int counter;
 		counter = 1;
@@ -13,3 +13,4 @@ public class doWhile {
 	}
 
 }
+// Do not forget to increase the variable used in the condition, otherwise the loop will never end!
