e3c791c0710c98781ef0858f712df87e7b1a4074
NickBwalley <nickbwalley@gmail.com>
2021-03-21 12:18:31 +0300

    for_each_loop

--- ForEachLoop/src/forEach/Main.java
+++ ForEachLoop/src/forEach/Main.java
@@ -1,22 +1,28 @@
 package forEach;
 import java.util.Scanner;
 public class Main {
-
+	// There is also a "for-each" loop, which is used exclusively to loop through elements in an array:
+	/*
+	 * Syntax
+	for (type variableName : arrayName) {
+	  // code block to be executed
+	}*/
 	public static void main(String[] args) {
 		printArray();
 	}
 	
-	//method to print out array elements using the foreach Loop
+	
+	//method to print out array elements using the for-each Loop
 	public static void printArray() {
-		Scanner nicky = new Scanner(System.in);
+		Scanner input = new Scanner(System.in);
 		int n;
 		System.out.println("Enter size of the Array: ");
-		n = nicky.nextInt();
+		n = input.nextInt();
 		int array[] = new int[n];
 		
 		System.out.println("Enter the Elements of the Array: ");
 		for (int i = 0; i < array.length; i++) {
-			array[i] = nicky.nextInt();
+			array[i] = input.nextInt();
 		}
 		
 		//printing Elements of the array(This is using an enhanced for loop in printing elements of the array)
@@ -24,6 +30,7 @@ public class Main {
 		for(int z: array) {
 			System.out.println(z);
 		}
+		input.close();
 	}
 
 }
