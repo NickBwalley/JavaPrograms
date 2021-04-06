106fee02c2b0c9ce751fd7c7f6206571c0d70071
NickBwalley <nickbwalley@gmail.com>
2021-04-06 19:11:05 +0300

    java GetUserInput

--- GetUserInput/src/getUserInput/MainClass.java
+++ GetUserInput/src/getUserInput/MainClass.java
@@ -1,7 +1,7 @@
 package getUserInput;
 
 import java.util.Scanner;
-public class Main {
+public class MainClass {
 	public static void main(String [] args) {
 		Scanner input = new Scanner(System.in);
 		System.out.println("What is your Name: ");
