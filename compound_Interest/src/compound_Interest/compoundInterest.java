0caf95381ef8dfa57557007173c35762cd6b4742
NickBwalley <nickbwalley@gmail.com>
2021-04-06 18:25:08 +0300

    java CompoundInterestProgram

--- compound_Interest/src/compound_Interest/compoundInterest.java
+++ compound_Interest/src/compound_Interest/compoundInterest.java
@@ -3,19 +3,21 @@ import java.util.Scanner;
 public class compoundInterest {
 
 	public static void main(String[] args) {
-		Scanner nicky = new Scanner(System.in);
+		Scanner input = new Scanner(System.in);
 		double amount;
 		double principal;
 		double rate;
 		System.out.println("Enter principal Amount: ");
-		principal = nicky.nextDouble();
+		principal = input.nextDouble();
 		System.out.println("Enter the Rate: ");
-		rate = nicky.nextDouble();
+		rate = input.nextDouble();
 		
 		for(int day = 1; day < 31; day++) {
 			amount = principal * Math.pow(1+rate, day);
 			System.out.println(day + " " + amount);
 		}
+		
+		input.close();
 		
 	}
 
