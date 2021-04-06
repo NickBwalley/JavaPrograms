0d3cd952f5b0c97893542fb7a0ec05f3931b1412
NickBwalley <nickbwalley@gmail.com>
2021-04-06 19:30:52 +0300

    java MethodsWithParameters

--- MethodsWithParameters/src/methodWithParameters/MainClass.java
+++ MethodsWithParameters/src/methodWithParameters/MainClass.java
@@ -1,6 +1,6 @@
 package methodWithParameters;
 import java.util.Scanner;
-public class Main {
+public class MainClass {
 
 	public static void main(String[] args) {
 		Scanner nicky = new Scanner(System.in);
--- MethodsWithParameters/src/settersAndGetters/MainClass.java
+++ MethodsWithParameters/src/settersAndGetters/MainClass.java
@@ -1,6 +1,6 @@
 package settersAndGetters;
 
-public class Main {
+public class MainClass {
 
 	public static void main(String[] args) {
 		ClassOne nicky = new ClassOne();
