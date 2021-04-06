c4d05eb2863d05e9f2cd5db775773add6880cbbc
NickBwalley <nickbwalley@gmail.com>
2021-04-06 19:34:14 +0300

    java OverloadedConstructors

--- OverLoadedConstuctors/src/overloadedConstructors/MainClass.java
+++ OverLoadedConstuctors/src/overloadedConstructors/MainClass.java
@@ -1,6 +1,6 @@
 package overloadedConstructors;
 
-public class Main {
+public class MainClass {
 
 	public static void main(String[] args) {
 		ConstructorOverloading nicky = new ConstructorOverloading();
