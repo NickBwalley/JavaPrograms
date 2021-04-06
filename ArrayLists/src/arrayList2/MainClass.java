348e666fc0d76f80d0b148c2f78a207d6b6793b6
NickBwalley <nickbwalley@gmail.com>
2021-04-06 13:16:00 +0300

    java ArrayLists

--- ArrayLists/src/arrayList2/MainClass.java
+++ ArrayLists/src/arrayList2/MainClass.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import arrayList3.NickFury;
 
-public class Main {
+public class MainClass {
 
 	public static void main(String[] args) {
 		//classArrayList();
