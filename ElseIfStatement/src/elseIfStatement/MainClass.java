8778c903fa91b86862c1e32cbe49da40fc08c592
NickBwalley <nickbwalley@gmail.com>
2021-04-06 19:04:39 +0300

    java ElseIfStatements

--- ElseIfStatement/src/elseIfStatement/MainClass.java
+++ ElseIfStatement/src/elseIfStatement/MainClass.java
@@ -1,6 +1,6 @@
 package elseIfStatement;
 import java.util.Scanner;
-public class Main {
+public class MainClass {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
