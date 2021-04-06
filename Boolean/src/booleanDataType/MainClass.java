79ac81d46dba980f83ac8f90c73f1f6f3d473254
NickBwalley <nickbwalley@gmail.com>
2021-04-06 17:59:52 +0300

    java BooleanDataType

--- Boolean/src/booleanDataType/MainClass.java
+++ Boolean/src/booleanDataType/MainClass.java
@@ -11,7 +11,7 @@ public class MainClass {
 		int x = 10;
 		int y = 9;
 		System.out.println(x > y); // returns true, because 10 is higher than 9
-		System.out.println();
+		System.out.println(); // newline
 		int z = 10;
 		System.out.println(z == 10); // returns true, because the value of x is equal to 10
 		System.out.println(10 == 15); // returns false, because 10 is not equal to 15
