3bb95280962aceea391e2081dd9c7a7619a1bcf9
NickBwalley <nickbwalley@gmail.com>
2021-04-06 13:01:39 +0300

    java ArraysGetRandomNumbers

--- ArraysGetRandomNumbers/src/arrayGetRandomNumbers/MainClass.java
+++ ArraysGetRandomNumbers/src/arrayGetRandomNumbers/MainClass.java
@@ -6,6 +6,8 @@ public class MainClass {
 		Random rand = new Random();
 		int freq[] = new int[7];
 		
+		// loop to count the frequency of a duplicate number when the dice is rolled 1000 times and counts
+		// dice has 6 sides so every die roll, it counts the frequency of a side which has been acquired
 		for(int roll=1; roll < 1000; roll++) {
 			++freq[1+rand.nextInt(6)];			
 		}
