88346ec57e11a884253a61d4687d86f3dee460fb
NickBwalley <nickbwalley@gmail.com>
2021-04-06 12:07:39 +0300

    java IfStatement

--- IfStatement/src/ifStatement/MainClass.java
+++ IfStatement/src/ifStatement/MainClass.java
@@ -3,16 +3,16 @@ import java.util.Scanner;
 public class MainClass {
 
 	public static void main(String[] args) {
-		Scanner rikki = new Scanner(System.in);
+		Scanner input = new Scanner(System.in);
 		int mark;
 		System.out.println("Enter your Mark: ");
-		mark = rikki.nextInt();
+		mark = input.nextInt();
 		if(mark < 50) {
 			System.out.println("Hey You have failed your Exam!.. You therefore have not qualified!");
 		}else {
 			System.out.println("Wow! Splendid! You have passed therefore you are qualified!");
 		}
-		rikki.close();
+		input.close();
 	}
 
 }
