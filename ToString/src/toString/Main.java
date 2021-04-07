2cdeb85aee7eeaaf8425d24ba55c8a98e14db684
NickBwalley <nickbwalley@gmail.com>
2021-04-07 11:53:22 +0300

    java ToString

--- ToString/src/toString/Main.java
+++ ToString/src/toString/Main.java
@@ -1,6 +1,6 @@
 package toString;
 
-public class MainClass {
+public class Main {
 	public static void main(String[] args) {
 		ToString strowman = new ToString(14, 05, 2020);
 		myBirthday nicky = new myBirthday(2, 9, 2000);
--- ToString/src/toString2/Main.java
+++ ToString/src/toString2/Main.java
@@ -1,6 +1,6 @@
 package toString2;
 
-public class MainClass {
+public class Main {
 
 	public static void main(String[] args) {
 		// creating an integer of value 10
