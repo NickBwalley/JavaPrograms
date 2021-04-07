1667c65ecff9e95a71128f820747c0993df0ab73
NickBwalley <nickbwalley@gmail.com>
2021-04-07 10:10:53 +0300

    java AbstractClassesAndMethods

--- AbstractClassesAndMethods/src/abstractClassesAndMethods/Main.java
+++ AbstractClassesAndMethods/src/abstractClassesAndMethods/Main.java
@@ -17,6 +17,8 @@ public class Main {
 //PROPERTIES OF AN ABSTRACT CLASS
 // Your cannot create an instance of an object of an abstract class but you can create a reference from
 // its derived classes (Abstract classes cannot be instantiated)
+// can have constructors and static method also
+// It can have final methods which will force the subclass not to change the body of the method.
 
 //PROPERTIES OF AN ABSTRACT METHOD
 // For you to declare a method abstract, the entire class needs to be an abstract class.
