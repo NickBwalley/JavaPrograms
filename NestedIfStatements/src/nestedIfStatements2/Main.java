c72a1cd716d5bc98cccf9f637b5502a9b3deb42e
NickBwalley <nickbwalley@gmail.com>
2021-04-07 12:04:10 +0300

    java NestedIfStatements

---
+++ NestedIfStatements/src/nestedIfStatements2/Main.java
@@ -0,0 +1,24 @@
+package nestedIfStatements2;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input  = new Scanner(System.in);
+        int age;
+        
+        System.out.println("Enter Your Age: ");
+        age = input.nextInt();
+        
+        if(age < 18)
+            System.out.println("Your Are Still a Kid!");
+        else
+            System.out.println("You Are Not a Kid!");
+            if(age > 50)
+                System.out.println("You are OLD!");
+            if(age > 75)
+                System.out.println("You are Really OLD!");
+        
+        input.close();    
+    }
+
+}
