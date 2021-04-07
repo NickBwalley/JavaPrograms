310a0c5e730dd9c4b114512aaa40bd0771983d38
NickBwalley <nickbwalley@gmail.com>
2021-04-07 12:07:15 +0300

    java ReturnMethodUsingPrintf

---
+++ ReturnMethodUsingPrintf/src/returnmethodusingprintf/Main.java
@@ -0,0 +1,11 @@
+package returnmethodusingprintf;
+
+public class Main {
+
+    public static void main(String[] args) {
+       NickFury nicky = new NickFury();
+       nicky.setName("Chelsea Lydia");
+       nicky.printCrap();
+    }
+
+}
